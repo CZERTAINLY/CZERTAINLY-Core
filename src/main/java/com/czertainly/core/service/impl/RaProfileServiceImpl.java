b73b4fec45d380df4df833d72fd6bd6fd4b105d8
3KeyPradeep <pradeep.saminathan@3key.company>
2021-12-22 12:38:06 +0100

    Update RaProfileServiceImpl.java

--- src/main/java/com/czertainly/core/service/impl/RaProfileServiceImpl.java
+++ src/main/java/com/czertainly/core/service/impl/RaProfileServiceImpl.java
@@ -243,7 +243,9 @@ public class RaProfileServiceImpl implements RaProfileService {
         entity.setDescription(dto.getDescription());
         entity.setAttributes(AttributeDefinitionUtils.serialize(attributes));
         entity.setAuthorityInstanceReference(authorityInstanceRef);
-        entity.setEnabled(dto.isEnabled() != null && dto.isEnabled());
+        if(dto.isEnabled() != null) {
+            entity.setEnabled(dto.isEnabled() != null && dto.isEnabled());
+        }
         entity.setAuthorityInstanceName(authorityInstanceRef.getName());
         return entity;
     }
