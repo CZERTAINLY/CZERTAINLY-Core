d9b8424984175bda06cdbdce996b9430d095b172
3KeyPradeep <pradeep.saminathan@3key.company>
2022-03-23 17:06:30 +0530

    Create V202203231330__certificate_history.sql

---
+++ src/main/resources/db/migration/V202203231330__certificate_history.sql
@@ -0,0 +1,23 @@
+create sequence certificate_event_history_id_seq start 1 increment 1;
+
+CREATE TABLE certificate_event_history (
+	"id" BIGINT NOT NULL,
+	"uuid" VARCHAR NOT NULL,
+	"i_cre" TIMESTAMP NOT NULL,
+	"i_upd" TIMESTAMP NOT NULL,
+	"i_author" VARCHAR NOT NULL,
+	"action" VARCHAR NOT NULL,
+	"status" VARCHAR NOT NULL,
+	"message" VARCHAR NOT NULL,
+	"additional_information" VARCHAR NULL DEFAULT NULL,
+	"certificate_id" BIGINT NOT NULL,
+	PRIMARY KEY ("id"),
+)
+;
+
+
+alter table if exists certificate_event_history
+    add constraint CERTIFICATE_HISTORY_TO_CERTIFICATE_KEY_1
+    foreign key (certificate_id)
+    references certificate
+    ON UPDATE NO ACTION ON DELETE CASCADE;
