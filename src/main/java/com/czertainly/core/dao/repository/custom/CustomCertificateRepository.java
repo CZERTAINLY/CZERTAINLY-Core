eb0903a715b520192ec013fa2154a160aec62689
3keyroman <roman.cinkais@3key.company>
2022-03-31 11:55:30 +0200

    Cleanup of unused imports

--- src/main/java/com/czertainly/core/dao/repository/custom/CustomCertificateRepository.java
+++ src/main/java/com/czertainly/core/dao/repository/custom/CustomCertificateRepository.java
@@ -1,7 +1,5 @@
 package com.czertainly.core.dao.repository.custom;
 
-import com.querydsl.core.types.Predicate;
-
 public interface CustomCertificateRepository {
 
     void bulkUpdateQuery(String query);
