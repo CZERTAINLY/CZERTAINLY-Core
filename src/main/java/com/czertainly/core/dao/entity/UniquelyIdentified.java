f2d509bd065c34d13f38cba7eb10ac6214e6d4b5
3KeyPradeep <pradeep.saminathan@3key.company>
2022-08-30 10:07:49 +0100

    Update UUID generation method
    Update UUID generation method from variable initialization to pre persist annotation

--- src/main/java/com/czertainly/core/dao/entity/UniquelyIdentified.java
+++ src/main/java/com/czertainly/core/dao/entity/UniquelyIdentified.java
@@ -2,13 +2,21 @@ package com.czertainly.core.dao.entity;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
 import java.util.UUID;
 
 @MappedSuperclass
 public abstract class UniquelyIdentified {
 
     @Column(name = "uuid")
-    protected String uuid = UUID.randomUUID().toString();
+    protected String uuid;
+
+    @PrePersist
+    private void generateUuid(){
+        if (uuid == null) {
+            setUuid(UUID.randomUUID().toString());
+        }
+    }
 
     public String getUuid() {
         return uuid;
