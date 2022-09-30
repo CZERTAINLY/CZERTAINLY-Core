bf3a0bdefe4679cdad463f777202712988695f2a
3KeyPradeep <pradeep.saminathan@3key.company>
2022-09-30 11:26:03 +0100

    Import optimization and code cleanup

--- src/test/java/com/czertainly/core/aop/AuditLogAspectTest.java
+++ src/test/java/com/czertainly/core/aop/AuditLogAspectTest.java
@@ -1,16 +1,12 @@
 package com.czertainly.core.aop;
 
 import com.czertainly.api.exception.NotFoundException;
-import com.czertainly.core.security.authz.SecuredUUID;
-import com.czertainly.core.util.BaseSpringBootTest;
 import com.czertainly.core.service.ClientOperationService;
 import com.czertainly.core.service.ConnectorService;
 import com.czertainly.core.util.BaseSpringBootTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.UUID;
 
 public class AuditLogAspectTest extends BaseSpringBootTest {
 
