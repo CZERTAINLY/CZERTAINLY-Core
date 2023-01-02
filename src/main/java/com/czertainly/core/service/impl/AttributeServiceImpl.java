b788ecef349cc927f917c9b97b413024708cb607
3KeyPradeep <pradeep.saminathan@3key.company>
2023-01-02 20:35:37 +0530

    Update AttributeServiceImpl.java

--- src/main/java/com/czertainly/core/service/impl/AttributeServiceImpl.java
+++ src/main/java/com/czertainly/core/service/impl/AttributeServiceImpl.java
@@ -318,7 +318,7 @@ public class AttributeServiceImpl implements AttributeService {
 
     @Override
     public List<ResponseAttributeDto> getCustomAttributesWithValues(UUID uuid, Resource resource) {
-        logger.info("Getting the custom attributes for {} with UUID: {}", resource, uuid);
+        logger.info("Getting the custom attributes for {} with UUID: {}", resource.getCode(), uuid);
         List<CustomAttribute> attributes = new ArrayList<>();
         for (AttributeContent2Object object : attributeContent2ObjectRepository.findByObjectUuidAndObjectType(uuid, resource)) {
             AttributeDefinition definition = object.getAttributeContent().getAttributeDefinition();
