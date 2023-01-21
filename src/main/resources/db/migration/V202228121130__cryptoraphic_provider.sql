7f19d3a91fbfd6fab8a0511a2b1b4693f28cb0d6
3KeyPradeep <pradeep.saminathan@3key.company>
2023-01-21 21:36:28 +0530

    Add Cryptography mandatory end points to the endpoint table
    Add the cryptography mandatory endpoint to the core endpoint table to validate the endpoints when a cryptography connector is connected

--- src/main/resources/db/migration/V202228121130__cryptoraphic_provider.sql
+++ src/main/resources/db/migration/V202228121130__cryptoraphic_provider.sql
@@ -123,4 +123,44 @@ alter table if exists certificate
 
 alter table certificate_group rename to certificate_key_group;
 
---TODO Add End points and function groups into the database for the Cryptographic Provider
+insert into function_group (name, code, uuid) values (
+    'cryptographyProvider',
+    'CRYPTOGRAPHY_PROVIDER',
+    'c2fece10-9896-11ed-a8fc-0242ac120002');
+
+-- add required endpoints for the compliance and rules management in the connector
+insert into endpoint (context,method,name,required,function_group_uuid,uuid) values
+    ('/v1/cryptographyProvider/tokens/{uuid}/keys/{keyUuid}/encrypt','POST','encryptData',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f78a72-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}/keys/random','POST','randomData',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f78db0-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}/activate','PATCH','activateTokenInstance',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f78ee6-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}/keys/{keyUuid}','GET','getKey',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f79044-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}/keys','GET','listKeys',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f792b0-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}/keys/pair/attributes','GET','listCreateKeyPairAttributes',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f79724-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}/tokenProfile/attributes/validate','POST','validateTokenProfileAttributes',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f7985a-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}/keys/pair','POST','createKeyPair',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f79a8a-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}/activate/attributes/validate','POST','validateTokenInstanceActivationAttributes',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f79ba2-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens','POST','createTokenInstance',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f79dfa-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}/keys/random/attributes','GET','listRandomAttributes',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f79efe-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}/keys/{keyUuid}/decrypt','POST','decryptData',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f7a0f2-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}','POST','updateTokenInstance',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f7a1ec-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/{kind}/attributes','GET','listAttributeDefinitions',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f7a5f2-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}/keys/secret/attributes/validate','POST','validateCreateSecretKeyAttributes',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f7a714-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}/keys/pair/attributes/validate','POST','validateCreateKeyPairAttributes',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f7a80e-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}/status','GET','getTokenInstanceStatus',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f7a908-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}/activate/attributes','GET','listTokenInstanceActivationAttributes',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f7ac14-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}','GET','getTokenInstance',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f7ad18-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}/tokenProfile/attributes','GET','listTokenProfileAttributes',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f7af20-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/callbacks/token/{option}/attributes','GET','getCreateTokenAttributes',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f7b13c-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}/keys/secret','POST','createSecretKey',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f7b240-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens','GET','listTokenInstances',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f7b344-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}/keys/random/attributes/validate','POST','validateRandomAttributes',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f7b43e-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}/keys/{keyUuid}/sign','POST','signData',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f7b6a0-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/{kind}/attributes/validate','POST','validateAttributes',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f7b7a4-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}/keys/{keyUuid}','DELETE','destroyKey',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f7b89e-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}/keys/{keyUuid}/verify','POST','verifyData',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f7b998-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}/deactivate','PATCH','deactivateTokenInstance',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f7baa6-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}/keys/secret/attributes','GET','listCreateSecretKeyAttributes',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f7bd9e-9896-11ed-a8fc-0242ac120002'),
+    ('/v1/cryptographyProvider/tokens/{uuid}','DELETE','removeTokenInstance',true,(select uuid from function_group where code = 'CRYPTOGRAPHY_PROVIDER'),'83f7beac-9896-11ed-a8fc-0242ac120002');
+
+-- Update discovery table connector UUID from String to UUID
+ALTER TABLE discovery_history ALTER COLUMN connector_uuid SET DATA TYPE UUID USING "connector_uuid"::UUID;
