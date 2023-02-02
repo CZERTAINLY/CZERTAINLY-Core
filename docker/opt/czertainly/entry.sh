4ca6d7645b03e3a10229bd0eb7fc78492d9c1c8e
3keyroman <roman.cinkais@3key.company>
2023-02-02 15:14:15 +0100

    Load trusted certificates at runtime

--- docker/opt/czertainly/entry.sh
+++ docker/opt/czertainly/entry.sh
@@ -3,18 +3,21 @@
 czertainlyHome="/opt/czertainly"
 source ${czertainlyHome}/static-functions
 
-if [ -f ${czertainlyHome}/trusted-certificates.pem ]
-then
-  log "INFO" "Adding additional trusted certificates to cacerts"
-  ./update-cacerts.sh /opt/czertainly/trusted-certificates.pem
+#if [ -f ${czertainlyHome}/trusted-certificates.pem ]
+#then
+#  log "INFO" "Adding additional trusted certificates to cacerts"
+#  ./update-cacerts.sh /opt/czertainly/trusted-certificates.pem
+#
+#  log "INFO" "Preparing truststore for the Core"
+#  ./prepare-truststore.sh /opt/czertainly/trusted-certificates.pem
+#
+#  log "INFO" "Launching the Core"
+#  java $JAVA_OPTS -jar ./app.jar
+#else
+#  log "ERROR" "No trusted certificates were provided!"
+#fi
 
-  log "INFO" "Preparing truststore for the Core"
-  ./prepare-truststore.sh /opt/czertainly/trusted-certificates.pem
-
-  log "INFO" "Launching the Core"
-  java $JAVA_OPTS -jar ./app.jar
-else
-  log "ERROR" "No trusted certificates were provided!"
-fi
+log "INFO" "Launching the Core"
+java -jar ./app.jar
 
 #exec "$@"
