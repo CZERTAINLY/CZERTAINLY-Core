43768c9aabd6a84c78fe58fb38b38d1dae3ef562
3keyroman <roman.cinkais@3key.company>
2023-02-03 16:48:38 +0100

    Add default trusted managers for the API clients

--- src/main/java/com/czertainly/core/config/ApplicationConfig.java
+++ src/main/java/com/czertainly/core/config/ApplicationConfig.java
@@ -11,6 +11,7 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.pqc.jcajce.provider.BouncyCastlePQCProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +20,7 @@ import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import javax.net.ssl.TrustManager;
 import java.security.Provider;
 import java.security.Security;
 
@@ -27,6 +29,10 @@ import java.security.Security;
 @PropertySource(value = ApplicationConfig.EXTERNAL_PROPERTY_SOURCE, ignoreResourceNotFound = true)
 @ComponentScan(basePackages = "com.czertainly.core")
 public class ApplicationConfig {
+
+    @Autowired
+    private TrustedCertificatesConfig trustedCertificatesConfig;
+
     public static final String EXTERNAL_PROPERTY_SOURCE =
             "file:${raprofiles-backend.config.dir:/etc/raprofiles-backend}/raprofiles-backend.properties";
 
@@ -71,63 +77,68 @@ public class ApplicationConfig {
     }
 
     @Bean
-    public HealthApiClient healthApiClient(WebClient webClient) {
-        return new HealthApiClient(webClient);
+    public TrustManager[] defaultTrustManagers() {
+        return trustedCertificatesConfig.getDefaultTrustManagers();
     }
 
     @Bean
-    public AttributeApiClient attributeApiClient(WebClient webClient) {
-        return new AttributeApiClient(webClient);
+    public HealthApiClient healthApiClient(WebClient webClient, TrustManager[] defaultTrustManagers) {
+        return new HealthApiClient(webClient, defaultTrustManagers);
     }
 
     @Bean
-    public ConnectorApiClient connectorApiClient(WebClient webClient) {
-        return new ConnectorApiClient(webClient);
+    public AttributeApiClient attributeApiClient(WebClient webClient, TrustManager[] defaultTrustManagers) {
+        return new AttributeApiClient(webClient, defaultTrustManagers);
     }
 
     @Bean
-    public AuthorityInstanceApiClient authorityInstanceApiClient(WebClient webClient) {
-        return new AuthorityInstanceApiClient(webClient);
+    public ConnectorApiClient connectorApiClient(WebClient webClient, TrustManager[] defaultTrustManagers) {
+        return new ConnectorApiClient(webClient, defaultTrustManagers);
     }
 
     @Bean
-    public EntityInstanceApiClient entityInstanceApiClient(WebClient webClient) {
-        return new EntityInstanceApiClient(webClient);
+    public AuthorityInstanceApiClient authorityInstanceApiClient(WebClient webClient, TrustManager[] defaultTrustManagers) {
+        return new AuthorityInstanceApiClient(webClient, defaultTrustManagers);
     }
 
     @Bean
-    public LocationApiClient locationApiClient(WebClient webClient) {
-        return new LocationApiClient(webClient);
+    public EntityInstanceApiClient entityInstanceApiClient(WebClient webClient, TrustManager[] defaultTrustManagers) {
+        return new EntityInstanceApiClient(webClient, defaultTrustManagers);
     }
 
     @Bean
-    public EndEntityProfileApiClient endEntityProfileApiClient(WebClient webClient) {
-        return new EndEntityProfileApiClient(webClient);
+    public LocationApiClient locationApiClient(WebClient webClient, TrustManager[] defaultTrustManagers) {
+        return new LocationApiClient(webClient, defaultTrustManagers);
     }
 
     @Bean
-    public EndEntityApiClient endEntityApiClient(WebClient webClient) {
-        return new EndEntityApiClient(webClient);
+    public EndEntityProfileApiClient endEntityProfileApiClient(WebClient webClient, TrustManager[] defaultTrustManagers) {
+        return new EndEntityProfileApiClient(webClient, defaultTrustManagers);
     }
 
     @Bean
-    public CertificateApiClient certificateApiClient(WebClient webClient) {
-        return new CertificateApiClient(webClient);
+    public EndEntityApiClient endEntityApiClient(WebClient webClient, TrustManager[] defaultTrustManagers) {
+        return new EndEntityApiClient(webClient, defaultTrustManagers);
     }
 
     @Bean
-    public DiscoveryApiClient discoveryApiClient(WebClient webClient) {
-        return new DiscoveryApiClient(webClient);
+    public CertificateApiClient certificateApiClient(WebClient webClient, TrustManager[] defaultTrustManagers) {
+        return new CertificateApiClient(webClient, defaultTrustManagers);
     }
 
     @Bean
-    public com.czertainly.api.clients.v2.CertificateApiClient certificateApiClientV2(WebClient webClient) {
-        return new com.czertainly.api.clients.v2.CertificateApiClient(webClient);
+    public DiscoveryApiClient discoveryApiClient(WebClient webClient, TrustManager[] defaultTrustManagers) {
+        return new DiscoveryApiClient(webClient, defaultTrustManagers);
     }
 
     @Bean
-    public ComplianceApiClient complianceApiClient(WebClient webClient) {
-        return new ComplianceApiClient(webClient);
+    public com.czertainly.api.clients.v2.CertificateApiClient certificateApiClientV2(WebClient webClient, TrustManager[] defaultTrustManagers) {
+        return new com.czertainly.api.clients.v2.CertificateApiClient(webClient, defaultTrustManagers);
+    }
+
+    @Bean
+    public ComplianceApiClient complianceApiClient(WebClient webClient, TrustManager[] defaultTrustManagers) {
+        return new ComplianceApiClient(webClient, defaultTrustManagers);
     }
 
     @Bean
@@ -147,17 +158,17 @@ public class ApplicationConfig {
 
     //Cryptographic API Clients
     @Bean
-    public TokenInstanceApiClient tokenInstanceApiClient(WebClient webClient) {
-        return new TokenInstanceApiClient(webClient);
+    public TokenInstanceApiClient tokenInstanceApiClient(WebClient webClient, TrustManager[] defaultTrustManagers) {
+        return new TokenInstanceApiClient(webClient, defaultTrustManagers);
     }
 
     @Bean
-    public KeyManagementApiClient keyManagementApiClient(WebClient webClient) {
-        return new KeyManagementApiClient(webClient);
+    public KeyManagementApiClient keyManagementApiClient(WebClient webClient, TrustManager[] defaultTrustManagers) {
+        return new KeyManagementApiClient(webClient, defaultTrustManagers);
     }
 
     @Bean
-    public CryptographicOperationsApiClient cryptographicOperationsApiClient(WebClient webClient) {
-        return new CryptographicOperationsApiClient(webClient);
+    public CryptographicOperationsApiClient cryptographicOperationsApiClient(WebClient webClient, TrustManager[] defaultTrustManagers) {
+        return new CryptographicOperationsApiClient(webClient, defaultTrustManagers);
     }
 }
