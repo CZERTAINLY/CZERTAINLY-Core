206486a9b344a5211fe5884e69fb327835ed2654
moro-lukasrejha <lukas.rejha@morosystems.cz>
2023-02-07 09:37:39 +0100

    Add more filter conditions to cryptography keys search and fix non functioning filters - added enum filtering by inner value

--- src/main/java/com/czertainly/core/util/converter/Sql2PredicateConverter.java
+++ src/main/java/com/czertainly/core/util/converter/Sql2PredicateConverter.java
@@ -1,10 +1,14 @@
 package com.czertainly.core.util.converter;
 
 import com.czertainly.api.model.client.certificate.SearchFilterRequestDto;
+import com.czertainly.api.model.connector.cryptography.enums.CryptographicAlgorithm;
+import com.czertainly.api.model.connector.cryptography.enums.IAbstractSearchableEnum;
 import com.czertainly.api.model.core.cryptography.key.KeyUsage;
+import com.czertainly.api.model.core.search.SearchCondition;
 import jakarta.persistence.criteria.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -33,7 +37,8 @@ public class Sql2PredicateConverter {
 
     private static Predicate processPredicate(final CriteriaBuilder criteriaBuilder, final Root root, final SearchFilterRequestDto dto, final Object valueObject) {
         Predicate predicate = null;
-        switch (dto.getCondition()) {
+        final SearchCondition searchCondition = checkOrReplaceSearchConfition(dto);
+        switch (searchCondition) {
             case EQUALS ->
                     predicate = criteriaBuilder.equal(prepareExpression(root, dto.getField().getCode()), prepareValue(dto, valueObject));
             case NOT_EQUALS ->
@@ -49,14 +54,24 @@ public class Sql2PredicateConverter {
                     criteriaBuilder.isNull(prepareExpression(root, dto.getField().getCode()))
             );
             case EMPTY -> predicate = criteriaBuilder.isNull(prepareExpression(root, dto.getField().getCode()));
-            case NOT_EMPTY ->
-                    predicate = criteriaBuilder.isNotNull(prepareExpression(root, dto.getField().getCode()));
+            case NOT_EMPTY -> predicate = criteriaBuilder.isNotNull(prepareExpression(root, dto.getField().getCode()));
             case GREATER ->
                     predicate = criteriaBuilder.greaterThan(prepareExpression(root, dto.getField().getCode()).as(Integer.class), Integer.parseInt(dto.getValue().toString()));
             case LESSER ->
                     predicate = criteriaBuilder.lessThan(prepareExpression(root, dto.getField().getCode()).as(Integer.class), Integer.parseInt(dto.getValue().toString()));
         }
         return predicate;
+    }
+
+    private static SearchCondition checkOrReplaceSearchConfition(final SearchFilterRequestDto dto) {
+        if (dto.getField().getEnumClass().equals(KeyUsage.class)) {
+            if (dto.getCondition().equals(SearchCondition.EQUALS)) {
+                return SearchCondition.CONTAINS;
+            } else if (dto.getCondition().equals(SearchCondition.NOT_EQUALS)) {
+                return SearchCondition.NOT_CONTAINS;
+            }
+        }
+        return dto.getCondition();
     }
 
     private static Expression<?> prepareExpression(final Root root, final String code) {
@@ -71,12 +86,16 @@ public class Sql2PredicateConverter {
     private static Object prepareValue(final SearchFilterRequestDto dto, final Object valueObject) {
         if (dto.getField().getEnumClass() != null) {
             if (dto.getField().getEnumClass().equals(KeyUsage.class)) {
-                final KeyUsage keyUsage = (KeyUsage) Enum.valueOf(dto.getField().getEnumClass(), valueObject.toString());
+                final KeyUsage keyUsage = (KeyUsage) findEnumByCustomValue(dto, valueObject);
                 return keyUsage.getId();
             }
-            return Enum.valueOf(dto.getField().getEnumClass(), valueObject.toString());
+            return findEnumByCustomValue(dto, valueObject);
         }
         return valueObject.toString();
+    }
+
+    private static Object findEnumByCustomValue(SearchFilterRequestDto dto, Object valueObject) {
+        return Arrays.stream(dto.getField().getEnumClass().getEnumConstants()).filter(enumValue -> enumValue.getEnumLabel().equals(valueObject.toString())).findFirst().get();
     }
 
     private static List<Object> readAndCheckIncomingValues(final SearchFilterRequestDto dto) {
