2be49a32924d39ce098851c16bd914ce3c820992
moro-lukasrejha <122088314+moro-lukasrejha@users.noreply.github.com>
2023-03-01 10:11:28 +0100

    Consistent filter on objects

---
+++ src/main/java/com/czertainly/core/util/SearchHelper.java
@@ -0,0 +1,23 @@
+package com.czertainly.core.util;
+
+import com.czertainly.api.model.core.search.SearchFieldDataDto;
+import com.czertainly.core.enums.SearchFieldNameEnum;
+
+public class SearchHelper {
+
+    public static SearchFieldDataDto prepareSearch(final SearchFieldNameEnum fieldNameEnum) {
+        return prepareSearch(fieldNameEnum, null);
+    }
+
+    public static SearchFieldDataDto prepareSearch(final SearchFieldNameEnum fieldNameEnum, final Object values) {
+        final SearchFieldDataDto fieldDataDto = new SearchFieldDataDto();
+        fieldDataDto.setField(fieldNameEnum.getFieldProperty());
+        fieldDataDto.setLabel(fieldNameEnum.getFieldLabel());
+        fieldDataDto.setMultiValue(fieldNameEnum.getFieldTypeEnum().isMultiValue());
+        fieldDataDto.setConditions(fieldNameEnum.getFieldTypeEnum().getContitions());
+        fieldDataDto.setType(fieldNameEnum.getFieldTypeEnum().getFieldType());
+        fieldDataDto.setValue(values);
+        return fieldDataDto;
+    }
+
+}
