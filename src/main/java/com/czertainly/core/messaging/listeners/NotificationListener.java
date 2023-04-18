94ba7a4380765a2567841678442b1554b8aa6c30
moro-lukasrejha <lukas.rejha@morosystems.cz>
2023-04-18 10:32:34 +0200

    CZERTAINLY-Core #369 Configure Rabbit MQ and use it to propagate certificate status events

---
+++ src/main/java/com/czertainly/core/messaging/listeners/NotificationListener.java
@@ -0,0 +1,22 @@
+package com.czertainly.core.messaging.listeners;
+
+import com.czertainly.core.messaging.model.EventMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificationListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(NotificationListener.class);
+
+    @RabbitListener(queues = "core.notifications", messageConverter = "jsonMessageConverter")
+    public void processMessage(EventMessage eventMessage) {
+        logger.info("Received notification message: {}", eventMessage);
+
+        // TODO process the message
+
+    }
+
+}
