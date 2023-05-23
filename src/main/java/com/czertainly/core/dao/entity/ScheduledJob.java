dd3e026b7e90b6ee0d83af83ab8d3dfcdd9eaddf
moro-lukasrejha <lukas.rejha@morosystems.cz>
2023-05-23 12:52:30 +0200

    CZERTAINLY-Interfaces #237 Add web interface for management of scheduled job - add table insertion script into core

---
+++ src/main/java/com/czertainly/core/dao/entity/ScheduledJob.java
@@ -0,0 +1,67 @@
+package com.czertainly.core.dao.entity;
+
+import com.czertainly.api.model.core.scheduler.ScheduledJobDetailDto;
+import com.czertainly.api.model.core.scheduler.ScheduledJobDto;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class ScheduledJob extends UniquelyIdentified{
+
+    @Column(name = "job_name")
+    private String jobName;
+
+    @Column(name = "cron_expression")
+    private String cronExpression;
+
+    @Column(name = "object_data", columnDefinition = "jsonb")
+    @JdbcTypeCode(SqlTypes.JSON)
+    private Object objectData;
+
+    @Column(name = "user_uuid")
+    private UUID userUuid;
+
+    @Column(name = "enabled")
+    private boolean enabled;
+
+    @Column(name = "one_shot_only")
+    private boolean oneShotOnly;
+
+    @Column(name = "system")
+    private boolean system;
+
+    @Column(name = "job_class_name")
+    private String jobClassName;
+
+    public ScheduledJobDetailDto mapToDetailDto() {
+        final ScheduledJobDetailDto dto = new ScheduledJobDetailDto();
+        dto.setUuid(this.uuid);
+        dto.setJobName(this.jobName);
+        dto.setCronExpression(this.cronExpression);
+        dto.setUserUuid(this.userUuid);
+        dto.setEnabled(this.enabled);
+        dto.setSystem(this.system);
+        dto.setOneShotOnly(this.oneShotOnly);
+        dto.setJobClassName(this.jobClassName);
+        return dto;
+    }
+
+    public ScheduledJobDto mapToDto() {
+        final ScheduledJobDto dto = new ScheduledJobDto();
+        dto.setUuid(this.uuid);
+        dto.setJobName(this.jobName);
+        dto.setCronExpression(this.cronExpression);
+        return dto;
+    }
+
+}
