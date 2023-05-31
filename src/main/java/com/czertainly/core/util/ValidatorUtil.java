d8a94f7093a5ac293e34c5dbd999ed79cf9a5386
moro-lukasrejha <122088314+moro-lukasrejha@users.noreply.github.com>
2023-05-31 14:09:15 +0200

    Add validation for ACME and SCEP profile name to contain only URL safe characters (#438)

--- src/main/java/com/czertainly/core/util/ValidatorUtil.java
+++ src/main/java/com/czertainly/core/util/ValidatorUtil.java
@@ -5,6 +5,8 @@ import com.czertainly.api.exception.ValidationException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.util.stream.Collectors;
+
 public class ValidatorUtil {
 
     public static void validateAuthToRaProfile(String raProfileName) throws ValidationException {
@@ -16,4 +18,19 @@ public class ValidatorUtil {
                     "Client identified by name {} does not have access to the RA Profile '{}'", authentication.getName(), raProfileName));
         }
     }
+
+    public static boolean containsUnreservedCharacters(final String value) {
+        return value.chars()
+                .filter(c -> !isUnreserved((char) c))
+                .mapToObj(c -> String.valueOf((char) c))
+                .collect(Collectors.joining()).length() > 0;
+    }
+
+    private static boolean isUnreserved(char c) {
+        return (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                (c >= '0' && c <= '9') ||
+                c == '-' || c == '.' || c == '_' || c == '~';
+    }
+
 }
