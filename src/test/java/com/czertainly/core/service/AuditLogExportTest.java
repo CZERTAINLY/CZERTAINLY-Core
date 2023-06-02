6781322cee39d678920fbe774394b9e7687d522f
David Maixner <david.maixner@morosystems.cz>
2023-06-02 15:23:40 +0200

    fix test

--- src/test/java/com/czertainly/core/service/AuditLogExportTest.java
+++ src/test/java/com/czertainly/core/service/AuditLogExportTest.java
@@ -37,7 +37,7 @@ public class AuditLogExportTest extends BaseSpringBootTest {
         auditLogService.logStartup();
         auditLogService.logShutdown();
         auditLogService.purgeAuditLogs(new AuditLogFilter(), Pageable.unpaged().getSort());
-        AuditLogResponseDto logs = auditLogService.listAuditLogs(new AuditLogFilter(), Pageable.unpaged());
+        AuditLogResponseDto logs = auditLogService.listAuditLogs(new AuditLogFilter(), Pageable.ofSize(10));
 
         // if the audit log is enabled, purging audit logs will keep the delete operation in the audit log
         // therefore we expect only 1 record to be available after purging all
