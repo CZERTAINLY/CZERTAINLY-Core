e2c70c9c8d75c865364acdec4147f9c8de092def
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2023-07-28 17:03:20 +0200

    Implement notifications settings

--- src/main/java/com/czertainly/core/service/impl/SettingServiceImpl.java
+++ src/main/java/com/czertainly/core/service/impl/SettingServiceImpl.java
@@ -1,6 +1,8 @@
 package com.czertainly.core.service.impl;
 
+import com.czertainly.api.model.connector.notification.NotificationType;
 import com.czertainly.api.model.core.auth.Resource;
+import com.czertainly.api.model.core.settings.NotificationSettingsDto;
 import com.czertainly.api.model.core.settings.PlatformSettingsDto;
 import com.czertainly.api.model.core.settings.SettingsSection;
 import com.czertainly.api.model.core.settings.UtilsSettingsDto;
@@ -9,20 +11,26 @@ import com.czertainly.core.dao.repository.SettingRepository;
 import com.czertainly.core.model.auth.ResourceAction;
 import com.czertainly.core.security.authz.ExternalAuthorization;
 import com.czertainly.core.service.SettingService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.management.Notification;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Service
 @Transactional
 public class SettingServiceImpl implements SettingService {
     public static final String UTILS_SERVICE_URL_NAME = "utilsServiceUrl";
+    public static final String NOTFICATIONS_MAPPING_NAME = "notificationsMapping";
 
     private static final Logger logger = LoggerFactory.getLogger(SettingServiceImpl.class);
 
@@ -59,7 +67,7 @@ public class SettingServiceImpl implements SettingService {
         // utils
         Setting setting = null;
         Map<String, Setting> utilsSettings = mappedSettings.get("utils");
-        if(utilsSettings == null || (setting = utilsSettings.get(UTILS_SERVICE_URL_NAME)) == null) {
+        if (utilsSettings == null || (setting = utilsSettings.get(UTILS_SERVICE_URL_NAME)) == null) {
             setting = new Setting();
             setting.setSection(SettingsSection.PLATFORM);
             setting.setCategory("utils");
@@ -67,6 +75,61 @@ public class SettingServiceImpl implements SettingService {
         }
 
         setting.setValue(platformSettings.getUtils().getUtilsServiceUrl());
+        settingRepository.save(setting);
+    }
+
+    @Override
+    public NotificationSettingsDto getNotificationSettings() {
+        List<Setting> settings = settingRepository.findBySection(SettingsSection.NOTIFICATIONS);
+        Map<NotificationType, String> valueMapped = null;
+        if (!settings.isEmpty()) {
+            String valueJson = settings.get(0).getValue();
+            ObjectMapper mapper = new ObjectMapper();
+            TypeReference<Map<NotificationType, String>> typeReference = new TypeReference<>() {
+            };
+
+            try {
+                valueMapped = mapper.readValue(valueJson, typeReference);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        NotificationSettingsDto notificationSettings = new NotificationSettingsDto();
+        notificationSettings.setNotificationsMapping(valueMapped);
+
+        return notificationSettings;
+    }
+
+    @Override
+    public void updateNotificationSettings(NotificationSettingsDto notificationSettings) {
+
+        List<Setting> settings = settingRepository.findBySection(SettingsSection.NOTIFICATIONS);
+        Setting setting;
+        if (settings.isEmpty()) {
+            setting = new Setting();
+            setting.setSection(SettingsSection.NOTIFICATIONS);
+            setting.setName(NOTFICATIONS_MAPPING_NAME);
+        } else {
+            setting = settings.get(0);
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        Map<NotificationType, String> notificationTypeStringMap = notificationSettings.getNotificationsMapping();
+        for (String uuid : notificationTypeStringMap.values()) {
+            try {
+                UUID.fromString(uuid);
+            }
+            catch (IllegalArgumentException e) {
+                logger.error("Invalid UUID of a notification reference.");
+                throw e;
+            }
+        }
+        try {
+            setting.setValue(mapper.writeValueAsString(notificationTypeStringMap));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         settingRepository.save(setting);
     }
 
