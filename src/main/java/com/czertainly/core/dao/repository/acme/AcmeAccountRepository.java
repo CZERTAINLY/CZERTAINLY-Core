21ff41a441635b9f93a8b91103c605111d2ac25c
3keyroman <roman.cinkais@3key.company>
2023-08-01 07:16:50 +0200

    Remove Transactional from ACME and SCEP Repositories

--- src/main/java/com/czertainly/core/dao/repository/acme/AcmeAccountRepository.java
+++ src/main/java/com/czertainly/core/dao/repository/acme/AcmeAccountRepository.java
@@ -4,12 +4,10 @@ import com.czertainly.core.dao.entity.acme.AcmeAccount;
 import com.czertainly.core.dao.repository.SecurityFilterRepository;
 import org.springframework.stereotype.Repository;
 
-import jakarta.transaction.Transactional;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-@Transactional
 public interface AcmeAccountRepository extends SecurityFilterRepository<AcmeAccount, Long> {
     Optional<AcmeAccount> findByUuid(UUID uuid);
     Optional<AcmeAccount> findByAccountId(String accountId);
--- src/main/java/com/czertainly/core/dao/repository/acme/AcmeOrderRepository.java
+++ src/main/java/com/czertainly/core/dao/repository/acme/AcmeOrderRepository.java
@@ -5,14 +5,12 @@ import com.czertainly.core.dao.entity.acme.AcmeOrder;
 import com.czertainly.core.dao.repository.SecurityFilterRepository;
 import org.springframework.stereotype.Repository;
 
-import jakarta.transaction.Transactional;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-@Transactional
 public interface AcmeOrderRepository extends SecurityFilterRepository<AcmeOrder, Long> {
     Optional<AcmeOrder> findByUuid(UUID uuid);
     Optional<AcmeOrder> findByOrderId(String orderId);
