81e41fc5dc5fc19fdec229b88dbd070ed23cfa27
3keyroman <roman.cinkais@3key.company>
2023-08-17 16:56:03 +0200

    Fix loading of lazy relations for ACME entities

--- src/main/java/com/czertainly/core/dao/repository/acme/AcmeChallengeRepository.java
+++ src/main/java/com/czertainly/core/dao/repository/acme/AcmeChallengeRepository.java
@@ -2,6 +2,7 @@ package com.czertainly.core.dao.repository.acme;
 
 import com.czertainly.core.dao.entity.acme.AcmeChallenge;
 import com.czertainly.core.dao.repository.SecurityFilterRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,6 +10,9 @@ import java.util.UUID;
 
 @Repository
 public interface AcmeChallengeRepository extends SecurityFilterRepository<AcmeChallenge, Long> {
+
     Optional<AcmeChallenge> findByUuid(UUID uuid);
+
+    @EntityGraph(attributePaths = {"authorization"})
     Optional<AcmeChallenge> findByChallengeId(String challengeId);
 }
