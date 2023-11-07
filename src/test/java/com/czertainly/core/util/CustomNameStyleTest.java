c696f4a4a1c8a7724d8ed692743d6216873fdb4c
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2023-11-07 14:59:40 +0100

    Update CustomNameStyleTest.java

--- src/test/java/com/czertainly/core/util/CustomNameStyleTest.java
+++ src/test/java/com/czertainly/core/util/CustomNameStyleTest.java
@@ -9,24 +9,26 @@ import org.slf4j.LoggerFactory;
 
 
 import javax.security.auth.x500.X500Principal;
+import java.security.cert.CertificateException;
 
 public class CustomNameStyleTest {
      private static final Logger logger = LoggerFactory.getLogger(CustomNameStyleTest.class);
-     private CertificateService certificateService;
+
     @Test
-    public void testCustomNameStyle() {
+    public void testCustomNameStyle() throws CertificateException {
+
         X500Principal x500Principal = new X500Principal("CN=Demo Client Sub CA,O=3Key Company s.r.o.");
-        logger.info(X500Name.getInstance(X500NameStyleCustom.INSTANCE, x500Principal.getEncoded()).toString());
+        logger.info(X500Name.getInstance(CzertainlyX500NameStyle.INSTANCE, x500Principal.getEncoded()).toString());
         x500Principal = new X500Principal("CN=Demo Client Sub CA,EMAIL=3key@company.com");
-        logger.info(X500Name.getInstance(X500NameStyleCustom.INSTANCE, x500Principal.getEncoded()).toString());
-        logger.info(X500Name.getInstance(X500NameStyleCustom.INSTANCE, new X500Principal("CN=Certificate Authority; O=Organization;L=Location, ST=State, C=US").getEncoded()).toString());
-        String string = X500Name.getInstance(X500NameStyleCustom.INSTANCE, new X500Principal("CN=Certificate Authority;O=Organization;L=Location,ST=State, C=US").getEncoded()).toString();
+        logger.info(X500Name.getInstance(CzertainlyX500NameStyle.INSTANCE, x500Principal.getEncoded()).toString());
+        logger.info(X500Name.getInstance(CzertainlyX500NameStyle.INSTANCE, new X500Principal("CN=Certificate Authority; O=Organization;L=Location, ST=State, C=US").getEncoded()).toString());
+        String string = X500Name.getInstance(CzertainlyX500NameStyle.INSTANCE, new X500Principal("CN=Certificate Authority;O=Organization;L=Location,ST=State, C=US").getEncoded()).toString();
         logger.info(string);
-        String stringMod = X500Name.getInstance(X500NameStyleCustom.INSTANCE, new X500Principal("CN=Certificate Authority; L=Location,ST=State, C=US;O=Organization").getEncoded()).toString();
+        String stringMod = X500Name.getInstance(CzertainlyX500NameStyle.INSTANCE, new X500Principal("CN=Certificate Authority; L=Location,ST=State, C=US;O=Organization").getEncoded()).toString();
         logger.info(stringMod);
-        String stringMod2 = X500Name.getInstance(X500NameStyleCustom.INSTANCE, new X500Principal("CN=Certificate Authority; O=Organization; L=Location,ST=State; C=US").getEncoded()).toString();
+        String stringMod2 = X500Name.getInstance(CzertainlyX500NameStyle.INSTANCE, new X500Principal("CN=Certificate Authority; O=Organization; L=Location,ST=State; C=US").getEncoded()).toString();
         logger.info(stringMod2);
-        logger.info(X500Name.getInstance(X500NameStyleCustom.INSTANCE, new X500Principal("CN=Example Root CA; O=Example Corp; EMAIL=admin@example.com,C=US").getEncoded()).toString());
+        logger.info(X500Name.getInstance(CzertainlyX500NameStyle.INSTANCE, new X500Principal("CN=Example Root CA; O=Example Corp; EMAIL=admin@example.com,C=US").getEncoded()).toString());
 //        logger.info(X500Name.getInstance(X500NameStyleCustom.INSTANCE, new X500Principal("E=3key@company.com, CN=Demo Client Sub CA").getEncoded()).toString());
         logger.info("========");
 //        new X500Principal("E=3key@company.com, CN=Demo Client Sub CA");
@@ -56,18 +58,24 @@ public class CustomNameStyleTest {
         for (String dnOrdering: dnOrderings2) {
             logger.info(new X500Principal(dnOrdering).toString());
             logger.info(ASN1Sequence.getInstance(new X500Principal(dnOrdering).getEncoded()).toString());
-            logger.info(getDnWithCustomStyle(dnOrdering));
+            logger.info("Default: " + getDnWithCustomStyle(dnOrdering));
+            logger.info("Normalized: " + getDnWithCustomStyleNormalized(dnOrdering));
 
         }
 
         logger.info(getDnWithCustomStyle("2.5.4.77=SSL Issuer; OU=IT Security; O=SecureCorp; L=City; ST=State; C=US"));
+        logger.info(getDnWithCustomStyleNormalized("2.5.4.77=SSL Issuer; OU=IT Security; O=SecureCorp; L=City; ST=State; C=US"));
 
         logger.info(getDnWithCustomStyle("CN=SSL Issuer; O=SecureCorp;OU=IT Security;L=City; ST=State; C=US; emailAddress=admin@example.com"));
         logger.info(getDnWithCustomStyle("CN=SSL Issuer; OU=IT Security;O=SecureCorp;L=City; ST=State; C=US; emailAddress=admin@example.com"));
 
         }
     private String getDnWithCustomStyle(String dn){
-        return X500Name.getInstance(X500NameStyleCustom.INSTANCE, new X500Principal(dn).getEncoded()).toString();
+        return X500Name.getInstance(CzertainlyX500NameStyle.DEFAULT_INSTANCE, new X500Principal(dn).getEncoded()).toString();
+    }
+
+    private String getDnWithCustomStyleNormalized(String dn){
+        return X500Name.getInstance(CzertainlyX500NameStyle.NORMALIZED_INSTANCE, new X500Principal(dn).getEncoded()).toString();
     }
     }
 
