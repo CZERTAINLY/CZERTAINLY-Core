aa125a5bf9dcb2f8d1d2458e49935cce33d60bf6
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2023-11-09 11:43:07 +0100

    Add csr migration

--- src/main/java/db/migration/V202311071500__IssuerAndSubjectDnMigration.java
+++ src/main/java/db/migration/V202311071500__IssuerAndSubjectDnMigration.java
@@ -1,19 +1,19 @@
 package db.migration;
 
 import com.czertainly.core.util.CertificateUtil;
+import com.czertainly.core.util.CsrUtil;
 import com.czertainly.core.util.CzertainlyX500NameStyle;
 import com.czertainly.core.util.DatabaseMigration;
 import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.pkcs.jcajce.JcaPKCS10CertificationRequest;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 
-import java.nio.charset.StandardCharsets;
 import java.security.cert.X509Certificate;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Base64;
 import java.util.List;
 
 /**
@@ -29,11 +29,11 @@ public class V202311071500__IssuerAndSubjectDnMigration extends BaseJavaMigratio
     public void migrate(Context context) throws Exception {
         try (Statement select = context.getConnection().createStatement()) {
             createColumn(context);
-            addPublicKeyFingerprint(context);
+            formatIssuerAndSubjectDn(context);
         }
     }
 
-    private void addPublicKeyFingerprint(Context context) throws Exception {
+    private void formatIssuerAndSubjectDn(Context context) throws Exception {
         try (Statement select = context.getConnection().createStatement()) {
             try (ResultSet rows = select.executeQuery("SELECT c.uuid, a.content FROM certificate c JOIN certificate_content a ON a.id = c.certificate_content_id")) {
                 List<String> commands = new ArrayList<>();
@@ -42,16 +42,31 @@ public class V202311071500__IssuerAndSubjectDnMigration extends BaseJavaMigratio
                     byte[] subjectDnPrincipalEncoded = certificate.getSubjectX500Principal().getEncoded();
                     byte[] issuerDnPrincipalEncoded = certificate.getIssuerX500Principal().getEncoded();
                     commands.add(
-                            "update certificate set issuer_dn = '" + X500Name.getInstance(CzertainlyX500NameStyle.DEFAULT_INSTANCE, issuerDnPrincipalEncoded).toString().replace("'", "''") + "' where uuid='" + rows.getString("uuid") + "'"
+                            "update certificate set issuer_dn = '" + X500Name.getInstance(CzertainlyX500NameStyle.DEFAULT, issuerDnPrincipalEncoded).toString().replace("'", "''") + "' where uuid='" + rows.getString("uuid") + "'"
                     );
                     commands.add(
-                            "update certificate set subject_dn = '" + X500Name.getInstance(CzertainlyX500NameStyle.DEFAULT_INSTANCE, subjectDnPrincipalEncoded).toString().replace("'", "''") + "' where uuid='" + rows.getString("uuid") + "'"
+                            "update certificate set subject_dn = '" + X500Name.getInstance(CzertainlyX500NameStyle.DEFAULT, subjectDnPrincipalEncoded).toString().replace("'", "''") + "' where uuid='" + rows.getString("uuid") + "'"
                     );
                     commands.add(
-                            "update certificate set issuer_dn_normalized = '" + X500Name.getInstance(CzertainlyX500NameStyle.NORMALIZED_INSTANCE, issuerDnPrincipalEncoded).toString().replace("'", "''") + "' where uuid='" + rows.getString("uuid") + "'"
+                            "update certificate set issuer_dn_normalized = '" + X500Name.getInstance(CzertainlyX500NameStyle.NORMALIZED, issuerDnPrincipalEncoded).toString().replace("'", "''") + "' where uuid='" + rows.getString("uuid") + "'"
                     );
                     commands.add(
-                            "update certificate set subject_dn_normalized = '" + X500Name.getInstance(CzertainlyX500NameStyle.NORMALIZED_INSTANCE, subjectDnPrincipalEncoded).toString().replace("'", "''") + "' where uuid='" + rows.getString("uuid") + "'"
+                            "update certificate set subject_dn_normalized = '" + X500Name.getInstance(CzertainlyX500NameStyle.NORMALIZED, subjectDnPrincipalEncoded).toString().replace("'", "''") + "' where uuid='" + rows.getString("uuid") + "'"
+                    );
+                }
+                executeCommands(select, commands);
+            }
+            try (ResultSet rows = select.executeQuery("SELECT c.uuid, c.content FROM certificate_request c")) {
+                List<String> commands = new ArrayList<>();
+                while (rows.next()) {
+                    final JcaPKCS10CertificationRequest jcaObject = CsrUtil.csrStringToJcaObject(rows.getString("content"));
+                    byte[] subjectDnPrincipalEncoded = jcaObject.getSubject().getEncoded();
+
+                    commands.add(
+                            "update certificate_request set subject_dn = '" + X500Name.getInstance(CzertainlyX500NameStyle.DEFAULT, subjectDnPrincipalEncoded).toString().replace("'", "''") + "' where uuid='" + rows.getString("uuid") + "'"
+                    );
+                    commands.add(
+                            "update certificate_request set subject_dn_normalized = '" + X500Name.getInstance(CzertainlyX500NameStyle.NORMALIZED, subjectDnPrincipalEncoded).toString().replace("'", "''") + "' where uuid='" + rows.getString("uuid") + "'"
                     );
                 }
                 executeCommands(select, commands);
@@ -63,8 +78,9 @@ public class V202311071500__IssuerAndSubjectDnMigration extends BaseJavaMigratio
     private void createColumn(Context context) throws Exception {
         String addIssuerDnNormalized = "ALTER TABLE certificate add column issuer_dn_normalized VARCHAR NULL DEFAULT NULL";
         String addSubjectDnNormalized = "ALTER TABLE certificate add column subject_dn_normalized VARCHAR NULL DEFAULT NULL";
+        String addSubjectDnNormalizedCsr = "ALTER TABLE certificate_request add column subject_dn_normalized VARCHAR NULL DEFAULT NULL";
         try (Statement select = context.getConnection().createStatement()) {
-            executeCommands(select, List.of(addIssuerDnNormalized, addSubjectDnNormalized));
+            executeCommands(select, List.of(addIssuerDnNormalized, addSubjectDnNormalized, addSubjectDnNormalizedCsr));
         }
     }
 
