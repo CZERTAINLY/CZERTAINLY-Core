83b70ce74104986a527e810ebb1e3ed9437f684f
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2023-11-10 15:18:49 +0100

    Add custom X500NameStyle to format and normalize Subject Distinguished Names (#589)

--- src/main/java/com/czertainly/core/service/CertificateService.java
+++ src/main/java/com/czertainly/core/service/CertificateService.java
@@ -39,7 +39,8 @@ public interface CertificateService extends ResourceExtensionService  {
     // TODO AUTH - unable to check access based on certificate serial number. Make private? Special permission? Call opa in method?
     Certificate getCertificateEntityByFingerprint(String fingerprint) throws NotFoundException;
 
-    Certificate getCertificateEntityByIssuerDnAndSerialNumber(String issuerDn, String serialNumber) throws NotFoundException;
+
+    Certificate getCertificateEntityByIssuerDnNormalizedAndSerialNumber(String issuerDn, String serialNumber) throws NotFoundException;
 
     Boolean checkCertificateExistsByFingerprint(String fingerprint);
 
