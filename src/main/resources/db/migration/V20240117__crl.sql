798d67c524abcf9da8b7048900c72599a928b7fb
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2024-01-19 11:24:20 +0100

    Edit migration

--- src/main/resources/db/migration/V20240117__crl.sql
+++ src/main/resources/db/migration/V20240117__crl.sql
@@ -1,27 +1,24 @@
-
 CREATE TABLE crl (
-	id BIGINT NOT NULL,
 	uuid UUID NOT NULL UNIQUE,
-	ca_certificate_uuid UUID DEFAULT NULL,
-	issuer_dn VARCHAR UNIQUE,
-	serial_number VARCHAR UNIQUE,
+	ca_certificate_uuid UUID,
+	issuer_dn VARCHAR,
+	serial_number VARCHAR,
 	crl_issuer_dn VARCHAR,
     crl_number VARCHAR,
     next_update TIMESTAMP,
     crl_number_delta VARCHAR,
     next_update_delta TIMESTAMP,
     last_revocation_date TIMESTAMP,
-	PRIMARY KEY (id),
+	PRIMARY KEY (uuid),
 	FOREIGN KEY (ca_certificate_uuid) REFERENCES certificate(uuid)
 );
 
 CREATE TABLE crl_entry (
-	id BIGINT NOT NULL,
 	crl_uuid UUID,
 	serial_number VARCHAR,
     revocation_date TIMESTAMP,
     revocation_reason VARCHAR,
-	PRIMARY KEY (id),
-	FOREIGN KEY (crl_uuid) REFERENCES crl(uuid)
+	PRIMARY KEY (crl_uuid, serial_number),
+	FOREIGN KEY (crl_uuid) REFERENCES crl(uuid) ON UPDATE CASCADE ON DELETE CASCADE
 );
-;
+
