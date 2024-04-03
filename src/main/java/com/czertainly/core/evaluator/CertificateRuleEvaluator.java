2bce9be39ef995db089f08d7196e8464253ebd79
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2024-04-03 15:11:07 +0200

    Create CertificateRuleEvaluator.java

---
+++ src/main/java/com/czertainly/core/evaluator/CertificateRuleEvaluator.java
@@ -0,0 +1,46 @@
+package com.czertainly.core.evaluator;
+
+import com.czertainly.api.exception.ActionException;
+import com.czertainly.api.exception.AttributeException;
+import com.czertainly.api.exception.CertificateOperationException;
+import com.czertainly.api.exception.NotFoundException;
+import com.czertainly.api.model.core.auth.Resource;
+import com.czertainly.api.model.core.rules.RuleActionType;
+import com.czertainly.api.model.core.search.FilterFieldSource;
+import com.czertainly.core.dao.entity.Certificate;
+import com.czertainly.core.dao.entity.RuleAction;
+import com.czertainly.core.security.authz.SecuredUUID;
+import com.czertainly.core.service.CertificateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("certificates")
+public class CertificateRuleEvaluator extends RuleEvaluator<Certificate> {
+
+    private CertificateService certificateService;
+
+    @Autowired
+    public void setCertificateService(CertificateService certificateService) {
+        this.certificateService = certificateService;
+    }
+
+    @Override
+    public void performAction(RuleAction action, Certificate object, Resource resource) throws CertificateOperationException, NotFoundException, AttributeException, ActionException {
+        if (action.getActionType() == RuleActionType.SET_FIELD & action.getFieldSource() == FilterFieldSource.PROPERTY) {
+            SecuredUUID certificateUuid = object.getSecuredUuid();
+            SecuredUUID propertyUuid = SecuredUUID.fromString(action.getActionData().toString());
+            switch (action.getFieldIdentifier()) {
+                case "raProfile":
+                    certificateService.switchRaProfile(certificateUuid, propertyUuid);
+                case "group":
+                    certificateService.updateCertificateGroup(object.getSecuredUuid(), propertyUuid);
+                case "owner":
+                    certificateService.updateOwner(certificateUuid, String.valueOf(propertyUuid), null);
+            }
+        }
+        super.performAction(action, object, Resource.CERTIFICATE);
+    }
+
+
+}
+
