a43896a4e4a6194d749f9caa3f758edfb22f303a
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2024-06-20 10:46:10 +0200

    Add certificate uuid to migration

--- src/main/resources/db/migration/V202406181200__create_protocol_certificate_association.sql
+++ src/main/resources/db/migration/V202406181200__create_protocol_certificate_association.sql
@@ -1,5 +1,6 @@
 CREATE TABLE certificate_protocol_association (
 	uuid UUID NOT NULL,
+	certificate_uuid UUID NOT NULL,
 	protocol TEXT NOT NULL,
 	protocol_profile_uuid UUID NOT NULL,
 	additional_protocol_uuid UUID,
