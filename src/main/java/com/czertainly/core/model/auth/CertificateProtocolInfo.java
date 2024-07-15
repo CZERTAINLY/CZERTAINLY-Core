2d7d06102e8817b5155e996f4f4c395aedbe227b
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2024-07-15 09:45:08 +0200

    Update CertificateProtocolInfo.java

--- src/main/java/com/czertainly/core/model/auth/CertificateProtocolInfo.java
+++ src/main/java/com/czertainly/core/model/auth/CertificateProtocolInfo.java
@@ -14,25 +14,25 @@ public class CertificateProtocolInfo {
     private UUID protocolProfileUuid;
     private UUID additionalProtocolUuid;
 
-    public static CertificateProtocolInfo Scep(UUID protocolProfileUuid) {
+    public static CertificateProtocolInfo Scep(UUID scepProfileUuid) {
         CertificateProtocolInfo certificateProtocolInfo = new CertificateProtocolInfo();
         certificateProtocolInfo.setProtocol(CertificateProtocol.SCEP);
-        certificateProtocolInfo.setProtocolProfileUuid(protocolProfileUuid);
+        certificateProtocolInfo.setProtocolProfileUuid(scepProfileUuid);
         return certificateProtocolInfo;
     }
 
-    public static CertificateProtocolInfo Acme(UUID protocolProfileUuid, UUID additionalProtocolUuid) {
+    public static CertificateProtocolInfo Acme(UUID acmeProfileUuid, UUID acmeAccountUuid) {
         CertificateProtocolInfo certificateProtocolInfo = new CertificateProtocolInfo();
         certificateProtocolInfo.setProtocol(CertificateProtocol.ACME);
-        certificateProtocolInfo.setProtocolProfileUuid(protocolProfileUuid);
-        certificateProtocolInfo.setAdditionalProtocolUuid(additionalProtocolUuid);
+        certificateProtocolInfo.setProtocolProfileUuid(acmeProfileUuid);
+        certificateProtocolInfo.setAdditionalProtocolUuid(acmeAccountUuid);
         return certificateProtocolInfo;
     }
 
-    public static CertificateProtocolInfo Cmp(UUID protocolProfileUuid) {
+    public static CertificateProtocolInfo Cmp(UUID cmpProfileUuid) {
         CertificateProtocolInfo certificateProtocolInfo = new CertificateProtocolInfo();
         certificateProtocolInfo.setProtocol(CertificateProtocol.CMP);
-        certificateProtocolInfo.setProtocolProfileUuid(protocolProfileUuid);
+        certificateProtocolInfo.setProtocolProfileUuid(cmpProfileUuid);
         return certificateProtocolInfo;
     }
 }
