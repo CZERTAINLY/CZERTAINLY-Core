1c398ced5b2f46d53912377e86affdfb8f3b3a6e
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2024-08-30 11:16:54 +0200

    Add info endpoint using Actuator

---
+++ src/main/java/com/czertainly/core/api/DatabaseInfoContributor.java
@@ -0,0 +1,35 @@
+package com.czertainly.core.api;
+
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class DatabaseInfoContributor implements InfoContributor {
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        String url = System.getenv("JDBC_URL");
+        String username = System.getenv("JDBC_USERNAME");
+        String password = System.getenv("JDBC_PASSWORD");
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            DatabaseMetaData metaData = connection.getMetaData();
+            Map<String, String> databaseDetails = new HashMap<>();
+            databaseDetails.put("system", metaData.getDatabaseProductName());
+            databaseDetails.put("version", metaData.getDatabaseProductVersion());
+            builder.withDetail("db", databaseDetails);
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to retrieve database information.", e);
+        }
+
+
+    }
+
+}
