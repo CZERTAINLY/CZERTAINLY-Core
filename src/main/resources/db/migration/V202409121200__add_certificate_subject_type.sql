229aefc5e9e4dfec5589ffccf764eed607439828
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2024-09-17 16:43:39 +0200

    Update V202409121200__add_certificate_subject_type.sql

--- src/main/resources/db/migration/V202409121200__add_certificate_subject_type.sql
+++ src/main/resources/db/migration/V202409121200__add_certificate_subject_type.sql
@@ -1,8 +1,10 @@
-ALTER TABLE certificate ADD COLUMN subject_type TEXT NOT NULL;
+ALTER TABLE certificate ADD COLUMN subject_type TEXT;
 
-UPDATE certificate SET subject_type = 'END_ENTITY' WHERE basic_constraints = 'Subject Type=End Entity' AND (subject_dn_normalized != issuer_dn_normalized);
+UPDATE certificate SET subject_type = 'END_ENTITY' WHERE (basic_constraints = 'Subject Type=End Entity' AND (subject_dn_normalized != issuer_dn_normalized)) OR basic_constraints IS NULL;
 UPDATE certificate SET subject_type = 'SELF_SIGNED_END_ENTITY' WHERE basic_constraints = 'Subject Type=End Entity' AND (subject_dn_normalized = issuer_dn_normalized);
 UPDATE certificate SET subject_type = 'INTERMEDIATE_CA' WHERE basic_constraints = 'Subject Type=CA' AND (subject_dn_normalized != issuer_dn_normalized);
 UPDATE certificate SET subject_type = 'ROOT_CA' WHERE basic_constraints = 'Subject Type=CA' AND (subject_dn_normalized = issuer_dn_normalized);
+
+ALTER TABLE certificate ALTER COLUMN subject_type SET NOT NULL;
 
 ALTER TABLE certificate DROP COLUMN basic_constraints;
