ac8d3ee01841652629fc837eba2a5c98bf2b7b20
3keyroman <roman.cinkais@3key.company>
2024-10-25 08:02:01 +0200

    Fix class name and remove contextPath variable

--- src/main/java/com/czertainly/core/auth/oauth2/LoginController.java
+++ src/main/java/com/czertainly/core/auth/oauth2/LoginController.java
@@ -3,23 +3,19 @@ package com.czertainly.core.auth.oauth2;
 import com.czertainly.core.service.SettingService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
 import java.util.List;
 
 @Controller
 public class LoginController {
-
-    @Value("${server.servlet.context-path}")
-    private String contextPath;
 
     private SettingService settingService;
 
@@ -55,7 +51,7 @@ public class LoginController {
     @GetMapping("/oauth2/authorization/{provider}/prepare")
     public void loginWithProvider(@PathVariable String provider, HttpServletResponse response, HttpServletRequest request) throws IOException {
         request.getSession().setMaxInactiveInterval(settingService.getOAuth2ProviderSettings(provider).getSessionMaxInactiveInterval());
-        response.sendRedirect(contextPath + "/oauth2/authorization/" + provider);
+        response.sendRedirect(ServletUriComponentsBuilder.fromCurrentContextPath().build().getPath() + "/oauth2/authorization/" + provider);
     }
 
 }
