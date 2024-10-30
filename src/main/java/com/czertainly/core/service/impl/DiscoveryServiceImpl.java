c412566ac7c0d04de0eb67d2a9f80e674e34fca9
Ľubomír Wassermann <76479559+lubomirw@users.noreply.github.com>
2024-10-30 23:34:34 +0100

    Register metadata content for discovered certificates before its processing

--- src/main/java/com/czertainly/core/service/impl/DiscoveryServiceImpl.java
+++ src/main/java/com/czertainly/core/service/impl/DiscoveryServiceImpl.java
@@ -11,6 +11,7 @@ import com.czertainly.api.model.client.discovery.DiscoveryHistoryDto;
 import com.czertainly.api.model.common.NameAndUuidDto;
 import com.czertainly.api.model.common.attribute.v2.AttributeType;
 import com.czertainly.api.model.common.attribute.v2.MetadataAttribute;
+import com.czertainly.api.model.common.attribute.v2.content.BaseAttributeContent;
 import com.czertainly.api.model.connector.discovery.DiscoveryDataRequestDto;
 import com.czertainly.api.model.connector.discovery.DiscoveryProviderCertificateDataDto;
 import com.czertainly.api.model.connector.discovery.DiscoveryProviderDto;
@@ -566,6 +567,7 @@ public class DiscoveryServiceImpl implements DiscoveryService {
         // categorize certs and collect metadata definitions
         Set<String> certMetadataUuids = new HashSet<>();
         List<MetadataAttribute> metadataDefinitions = new ArrayList<>();
+        Map<String, Set<BaseAttributeContent>> metadataContentsMapping = new HashMap<>();
         List<DiscoveryProviderCertificateDataDto> discoveredCertificates = new ArrayList<>();
         response.getCertificateData().forEach(c -> {
             if (uniqueCertificateContents.contains(c.getBase64Content())) {
@@ -576,15 +578,19 @@ public class DiscoveryServiceImpl implements DiscoveryService {
             }
 
             for (MetadataAttribute m : c.getMeta()) {
-                if (!certMetadataUuids.contains(m.getUuid())) {
+                Set<BaseAttributeContent> metadataContents = metadataContentsMapping.get(m.getUuid());
+                if (metadataContents == null) {
                     metadataDefinitions.add(m);
-                    certMetadataUuids.add(m.getUuid());
+                    metadataContents = new HashSet<>();
+                    metadataContentsMapping.put(m.getUuid(), metadataContents);
                 }
+
+                metadataContents.addAll(m.getContent());
             }
         });
 
         // add/update certificate metadata to prevent creating duplicate definitions in parallel processing
-        certificateHandler.updateMetadataDefinition(metadataDefinitions, connector.getUuid(), connector.getName());
+        certificateHandler.updateMetadataDefinition(metadataDefinitions, metadataContentsMapping, connector.getUuid(), connector.getName());
 
         // run in separate virtual thread and continue
         return executor.submit(() -> {
