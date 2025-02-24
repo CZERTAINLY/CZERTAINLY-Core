0dc2d5d5425f413e4a320866b3e092d7fb02ea7e
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2025-02-24 14:04:05 +0100

    Remove deprecated headers converter

--- src/main/java/com/czertainly/core/config/OAuth2Config.java
+++ src/main/java/com/czertainly/core/config/OAuth2Config.java
@@ -1,25 +1,52 @@
 package com.czertainly.core.config;
 
-import com.czertainly.core.auth.oauth2.CzertainlyOAuth2TokenRequestHeadersConverter;
+import com.czertainly.core.security.authn.CzertainlyAuthenticationException;
+import com.czertainly.core.util.OAuth2Constants;
+import com.nimbusds.jwt.SignedJWT;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
-import org.springframework.security.oauth2.client.endpoint.DefaultRefreshTokenTokenResponseClient;
-import org.springframework.security.oauth2.client.endpoint.OAuth2RefreshTokenGrantRequestEntityConverter;
+import org.springframework.security.oauth2.client.endpoint.AbstractOAuth2AuthorizationGrantRequest;
+import org.springframework.security.oauth2.client.endpoint.OAuth2RefreshTokenGrantRequest;
+import org.springframework.security.oauth2.client.endpoint.RestClientRefreshTokenTokenResponseClient;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.text.ParseException;
 
 @Configuration
 public class OAuth2Config {
     @Bean
     public OAuth2AuthorizedClientProvider oAuth2AuthorizedClientProvider() {
-        DefaultRefreshTokenTokenResponseClient refreshTokenTokenResponseClient = new DefaultRefreshTokenTokenResponseClient();
-        OAuth2RefreshTokenGrantRequestEntityConverter refreshTokenGrantRequestEntityConverter = new OAuth2RefreshTokenGrantRequestEntityConverter();
-        refreshTokenGrantRequestEntityConverter.setHeadersConverter(CzertainlyOAuth2TokenRequestHeadersConverter.withCharsetUtf8());
-        refreshTokenTokenResponseClient.setRequestEntityConverter(refreshTokenGrantRequestEntityConverter);
+        RestClientRefreshTokenTokenResponseClient refreshTokenTokenResponseClient = new RestClientRefreshTokenTokenResponseClient();
+        refreshTokenTokenResponseClient.addHeadersConverter(
+                this::convertProxyHeaders
+        );
 
         return OAuth2AuthorizedClientProviderBuilder.builder()
-                .refreshToken(configurer -> configurer.accessTokenResponseClient(refreshTokenTokenResponseClient))
+                .refreshToken(configure -> configure.accessTokenResponseClient(refreshTokenTokenResponseClient))
                 .build();
+    }
+
+    private HttpHeaders convertProxyHeaders(AbstractOAuth2AuthorizationGrantRequest grantRequest) {
+        HttpHeaders headers = new HttpHeaders();
+        ClientRegistration clientRegistration = grantRequest.getClientRegistration();
+        if (clientRegistration.getRegistrationId().equals(OAuth2Constants.INTERNAL_OAUTH2_PROVIDER_RESERVED_NAME)) {
+            URI issuerUrl;
+            try {
+                String issuerUrlString = SignedJWT.parse(((OAuth2RefreshTokenGrantRequest) grantRequest).getAccessToken().getTokenValue()).getJWTClaimsSet().getIssuer();
+                issuerUrl = new URI(issuerUrlString);
+            } catch (URISyntaxException | ParseException e) {
+                throw new CzertainlyAuthenticationException("Could not parse issuer URL in token: " + e.getMessage());
+            }
+            headers.set("X-Forwarded-Host", issuerUrl.getHost());
+            if (issuerUrl.getPort() > 0) headers.set("X-Forwarded-Port", String.valueOf(issuerUrl.getPort()));
+            headers.set("X-Forwarded-Proto", issuerUrl.getScheme());
+        }
+        return headers;
     }
 
 }
