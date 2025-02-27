fe56a16e31a7643081d4fee5ddd568e46655ea61
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2025-02-27 09:41:44 +0100

    Remove import

--- src/main/java/com/czertainly/core/config/ProtocolValidationFilter.java
+++ src/main/java/com/czertainly/core/config/ProtocolValidationFilter.java
@@ -6,7 +6,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
