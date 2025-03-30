cbf1cd798eb8569b8298ad524ab52c8f9005ec98
3keyroman <roman.cinkais@3key.company>
2025-03-30 10:44:35 +0200

    Update CertificateUtilTest.java

--- src/test/java/com/czertainly/core/util/CertificateUtilTest.java
+++ src/test/java/com/czertainly/core/util/CertificateUtilTest.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class CertificateUtilTest {
+class CertificateUtilTest {
 
     private static final String VALID_SAN_STRING = "{\"dNSName\":[\"czertainly.com\"],\"directoryName\":[],\"ediPartyName\":[],\"iPAddress\":[\"192.168.10.10\"],\"otherName\":[\"1.2.3.4=example othername\"],\"registeredID\":[],\"rfc822Name\":[],\"uniformResourceIdentifier\":[],\"x400Address\":[]}";
 
@@ -24,7 +24,7 @@ public class CertificateUtilTest {
     );
 
     @Test
-    public void testSerializeSans() {
+    void testSerializeSans() {
         String result = CertificateUtil.serializeSans(VALID_SAN_MAP);
         Assertions.assertEquals(VALID_SAN_STRING, result);
 
@@ -36,7 +36,7 @@ public class CertificateUtilTest {
     }
 
     @Test
-    public void testDeserializeSans() {
+    void testDeserializeSans() {
         Map<String, List<String>> result = CertificateUtil.deserializeSans(VALID_SAN_STRING);
         Assertions.assertEquals(VALID_SAN_MAP, result);
 
@@ -48,13 +48,13 @@ public class CertificateUtilTest {
     }
 
     @Test
-    public void testInvalidDeserializeSans() {
+    void testInvalidDeserializeSans() {
         String invalidJson = "{invalid json}";
         Assertions.assertThrows(IllegalStateException.class, () -> CertificateUtil.deserializeSans(invalidJson));
     }
 
     @Test
-    public void testInvalidSerializeSans() {
+    void testInvalidSerializeSans() {
         Map<String, List<String>> invalidMap = new HashMap<>();
         invalidMap.put("invalidKey", null);
 
