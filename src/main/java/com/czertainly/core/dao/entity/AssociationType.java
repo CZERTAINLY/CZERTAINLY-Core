d1db69e6e958b9a19798bee9552255e91d74928c
Ľubomír Wassermann <76479559+lubomirw@users.noreply.github.com>
2025-04-15 16:30:25 +0200

    Update PQC tests and add Falcon CSR generation
    - add assertions to test public key sizes

--- src/main/java/com/czertainly/core/dao/entity/AssociationType.java
+++
@@ -1,6 +0,0 @@
-package com.czertainly.core.dao.entity;
-
-public enum AssociationType {
-    GROUP,
-    OWNER
-}
--- src/test/java/com/czertainly/core/cryptography/PQCTests.java
+++ src/test/java/com/czertainly/core/cryptography/PQCTests.java
@@ -1,5 +1,6 @@
 package com.czertainly.core.cryptography;
 
+import com.czertainly.core.util.KeySizeUtil;
 import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.jcajce.spec.MLDSAParameterSpec;
 import org.bouncycastle.jcajce.spec.SLHDSAParameterSpec;
@@ -14,14 +15,21 @@ import org.bouncycastle.pkcs.PKCS10CertificationRequest;
 import org.bouncycastle.pkcs.PKCS10CertificationRequestBuilder;
 import org.bouncycastle.pkcs.PKCSException;
 import org.bouncycastle.pkcs.jcajce.JcaPKCS10CertificationRequestBuilder;
+import org.bouncycastle.pqc.jcajce.provider.BouncyCastlePQCProvider;
+import org.bouncycastle.pqc.jcajce.spec.FalconParameterSpec;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.security.*;
 import java.security.spec.AlgorithmParameterSpec;
+import java.util.Objects;
 
+@SpringBootTest
 class PQCTests {
+
+    public static final String FALCON = "Falcon";
 
     public static final String PURE_ML_DSA = "ML-DSA";
     public static final String HASH_ML_DSA = "HASH-ML-DSA";
@@ -52,69 +60,79 @@ class PQCTests {
         Assertions.assertTrue(signAndVerify(PURE_SLH_DSA, SLHDSAParameterSpec.slh_dsa_shake_128s, PURE_SLH_DSA));
         Assertions.assertTrue(signAndVerify(PURE_SLH_DSA, SLHDSAParameterSpec.slh_dsa_shake_128s, HASH_SLH_DSA));
         Assertions.assertTrue(signAndVerify(PURE_SLH_DSA, SLHDSAParameterSpec.slh_dsa_sha2_128f_with_sha256, HASH_SLH_DSA));
-        Assertions.assertTrue(signAndVerify(PURE_SLH_DSA, SLHDSAParameterSpec.slh_dsa_sha2_128f_with_sha256,  SLHDSAParameterSpec.slh_dsa_sha2_128f_with_sha256.getName()));
+        Assertions.assertTrue(signAndVerify(PURE_SLH_DSA, SLHDSAParameterSpec.slh_dsa_sha2_128f_with_sha256, SLHDSAParameterSpec.slh_dsa_sha2_128f_with_sha256.getName()));
         Assertions.assertThrows(Exception.class, () -> signAndVerify(PURE_SLH_DSA, SLHDSAParameterSpec.slh_dsa_sha2_128f_with_sha256, PURE_SLH_DSA));
     }
 
+    @Test
+    void testFalconCsr() throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException, OperatorCreationException, PKCSException {
+        PKCS10CertificationRequest csr = generateCsr(FALCON, FalconParameterSpec.falcon_512, FALCON + "-512", 7176);
+        Assertions.assertEquals(FALCON.toUpperCase(), new DefaultAlgorithmNameFinder().getAlgorithmName(csr.getSignatureAlgorithm()));
+        Assertions.assertTrue(verifyCsr(csr));
+
+        csr = generateCsr(FALCON, FalconParameterSpec.falcon_1024, FALCON + "-1024", 14344);
+        Assertions.assertEquals(FALCON.toUpperCase(), new DefaultAlgorithmNameFinder().getAlgorithmName(csr.getSignatureAlgorithm()));
+        Assertions.assertTrue(verifyCsr(csr));
+    }
 
     @Test
     void testMLDSACsr() throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException, OperatorCreationException, PKCSException {
-        PKCS10CertificationRequest csr = generateCsr(PURE_ML_DSA, MLDSAParameterSpec.ml_dsa_44, PURE_ML_DSA);
+        PKCS10CertificationRequest csr = generateCsr(PURE_ML_DSA, MLDSAParameterSpec.ml_dsa_44, PURE_ML_DSA, 10496);
         Assertions.assertEquals(MLDSAParameterSpec.ml_dsa_44.getName(), new DefaultAlgorithmNameFinder().getAlgorithmName(csr.getSignatureAlgorithm()));
         Assertions.assertTrue(verifyCsr(csr));
 
         // When generating CSR with key that does not have hash in parameters, HASH-ML-DSA is overwritten to ML-DSA
-        csr = generateCsr(PURE_ML_DSA, MLDSAParameterSpec.ml_dsa_44, HASH_ML_DSA);
+        csr = generateCsr(PURE_ML_DSA, MLDSAParameterSpec.ml_dsa_44, HASH_ML_DSA, 10496);
         Assertions.assertEquals(MLDSAParameterSpec.ml_dsa_44.getName(), new DefaultAlgorithmNameFinder().getAlgorithmName(csr.getSignatureAlgorithm()));
         Assertions.assertTrue(verifyCsr(csr));
 
-        csr = generateCsr(PURE_ML_DSA, MLDSAParameterSpec.ml_dsa_44_with_sha512, HASH_ML_DSA);
+        csr = generateCsr(PURE_ML_DSA, MLDSAParameterSpec.ml_dsa_44_with_sha512, HASH_ML_DSA, 10496);
         Assertions.assertEquals(MLDSAParameterSpec.ml_dsa_44_with_sha512.getName(), new DefaultAlgorithmNameFinder().getAlgorithmName(csr.getSignatureAlgorithm()));
         Assertions.assertTrue(verifyCsr(csr));
 
-        csr = generateCsr(PURE_ML_DSA, MLDSAParameterSpec.ml_dsa_44_with_sha512, PURE_ML_DSA);
+        csr = generateCsr(PURE_ML_DSA, MLDSAParameterSpec.ml_dsa_44_with_sha512, PURE_ML_DSA, 10496);
         Assertions.assertEquals(MLDSAParameterSpec.ml_dsa_44_with_sha512.getName(), new DefaultAlgorithmNameFinder().getAlgorithmName(csr.getSignatureAlgorithm()));
         Assertions.assertTrue(verifyCsr(csr));
 
-        csr = generateCsr(PURE_ML_DSA, MLDSAParameterSpec.ml_dsa_44_with_sha512, MLDSAParameterSpec.ml_dsa_44_with_sha512.getName());
+        csr = generateCsr(PURE_ML_DSA, MLDSAParameterSpec.ml_dsa_44_with_sha512, MLDSAParameterSpec.ml_dsa_44_with_sha512.getName(), 10496);
         Assertions.assertEquals(MLDSAParameterSpec.ml_dsa_44_with_sha512.getName(), new DefaultAlgorithmNameFinder().getAlgorithmName(csr.getSignatureAlgorithm()));
         Assertions.assertTrue(verifyCsr(csr));
 
-        csr = generateCsr(PURE_ML_DSA, MLDSAParameterSpec.ml_dsa_44_with_sha512, MLDSAParameterSpec.ml_dsa_44_with_sha512.getName());
+        csr = generateCsr(PURE_ML_DSA, MLDSAParameterSpec.ml_dsa_44_with_sha512, MLDSAParameterSpec.ml_dsa_44_with_sha512.getName(), 10496);
         Assertions.assertEquals(MLDSAParameterSpec.ml_dsa_44_with_sha512.getName(), new DefaultAlgorithmNameFinder().getAlgorithmName(csr.getSignatureAlgorithm()));
         Assertions.assertTrue(verifyCsr(csr));
 
-        Assertions.assertThrows(OperatorCreationException.class, () -> generateCsr(PURE_ML_DSA, MLDSAParameterSpec.ml_dsa_44, MLDSAParameterSpec.ml_dsa_44_with_sha512.getName()));
+        Assertions.assertThrows(OperatorCreationException.class, () -> generateCsr(PURE_ML_DSA, MLDSAParameterSpec.ml_dsa_44, MLDSAParameterSpec.ml_dsa_44_with_sha512.getName(), 10496));
     }
 
     @Test
     void testSLHDSACsr() throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException, OperatorCreationException, PKCSException {
-        PKCS10CertificationRequest csr = generateCsr(PURE_SLH_DSA, SLHDSAParameterSpec.slh_dsa_shake_128s, PURE_SLH_DSA);
+        PKCS10CertificationRequest csr = generateCsr(PURE_SLH_DSA, SLHDSAParameterSpec.slh_dsa_shake_128s, PURE_SLH_DSA, 256);
         Assertions.assertEquals(SLHDSAParameterSpec.slh_dsa_shake_128s.getName(), new DefaultAlgorithmNameFinder().getAlgorithmName(csr.getSignatureAlgorithm()));
         Assertions.assertTrue(verifyCsr(csr));
 
         // When generating CSR with key that does not have hash in parameters, HASH-ML-DSA is overwritten to ML-DSA
-        csr = generateCsr(PURE_SLH_DSA, SLHDSAParameterSpec.slh_dsa_shake_128s, HASH_SLH_DSA);
+        csr = generateCsr(PURE_SLH_DSA, SLHDSAParameterSpec.slh_dsa_shake_128s, HASH_SLH_DSA, 256);
         Assertions.assertEquals(SLHDSAParameterSpec.slh_dsa_shake_128s.getName(), new DefaultAlgorithmNameFinder().getAlgorithmName(csr.getSignatureAlgorithm()));
         Assertions.assertTrue(verifyCsr(csr));
 
-        csr = generateCsr(PURE_SLH_DSA, SLHDSAParameterSpec.slh_dsa_shake_128s_with_shake128, HASH_SLH_DSA);
+        csr = generateCsr(PURE_SLH_DSA, SLHDSAParameterSpec.slh_dsa_shake_128s_with_shake128, HASH_SLH_DSA, 256);
         Assertions.assertEquals(SLHDSAParameterSpec.slh_dsa_shake_128s_with_shake128.getName(), new DefaultAlgorithmNameFinder().getAlgorithmName(csr.getSignatureAlgorithm()));
         Assertions.assertTrue(verifyCsr(csr));
 
-        csr = generateCsr(PURE_SLH_DSA, SLHDSAParameterSpec.slh_dsa_shake_128s_with_shake128, HASH_SLH_DSA);
+        csr = generateCsr(PURE_SLH_DSA, SLHDSAParameterSpec.slh_dsa_shake_128s_with_shake128, HASH_SLH_DSA, 256);
         Assertions.assertEquals(SLHDSAParameterSpec.slh_dsa_shake_128s_with_shake128.getName(), new DefaultAlgorithmNameFinder().getAlgorithmName(csr.getSignatureAlgorithm()));
         Assertions.assertTrue(verifyCsr(csr));
     }
 
 
-
-
-    private static PKCS10CertificationRequest generateCsr(String keyAlgorithm, AlgorithmParameterSpec keyParameters, String signatureAlgorithm) throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException, OperatorCreationException {
+    private static PKCS10CertificationRequest generateCsr(String keyAlgorithm, AlgorithmParameterSpec keyParameters, String signatureAlgorithm, int expectedKeySize) throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException, OperatorCreationException {
         KeyPair keyPair = getKeyPair(keyAlgorithm, keyParameters);
+        Assertions.assertEquals(expectedKeySize, KeySizeUtil.getKeyLength(keyPair.getPublic()));
+
         X500Name name = new X500Name("CN=cn");
         ContentSigner signer = new JcaContentSignerBuilder(signatureAlgorithm)
-                .setProvider(BouncyCastleProvider.PROVIDER_NAME).build(keyPair.getPrivate());
+                .setProvider(Objects.equals(keyAlgorithm, FALCON) ? BouncyCastlePQCProvider.PROVIDER_NAME : BouncyCastleProvider.PROVIDER_NAME).build(keyPair.getPrivate());
         PKCS10CertificationRequestBuilder p10Builder = new JcaPKCS10CertificationRequestBuilder(
                 name,
                 keyPair.getPublic());
