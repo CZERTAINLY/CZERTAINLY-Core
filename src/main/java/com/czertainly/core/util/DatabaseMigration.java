d5cc0b3dea9c437d879af4302016617d0c609f86
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2025-05-12 17:25:59 +0200

    Change migration

--- src/main/java/com/czertainly/core/util/DatabaseMigration.java
+++ src/main/java/com/czertainly/core/util/DatabaseMigration.java
@@ -63,8 +63,7 @@ public class DatabaseMigration {
         V202408231055__DuplicateCertificates(-1714171989),
         V202411141900__CreateLocalhostUserAndPermissions(1923994081),
         V202501281511__LinkKeysToCertificates(-1169157739),
-        V202501281641__DuplicateCertificateContents(492594371),
-        V202507051200__DeactivateTokenWithDeprecatedKeyAlgorithms(596573861)
+        V202501281641__DuplicateCertificateContents(492594371)
         ;
 
         private final int checksum;
