56f7951e7ddd14d7592dc264be90765ecfc8deef
3keyroman <46850604+3keyroman@users.noreply.github.com>
2025-05-21 12:45:29 +0200

    Update SettingServiceTest.java

--- src/test/java/com/czertainly/core/service/SettingServiceTest.java
+++ src/test/java/com/czertainly/core/service/SettingServiceTest.java
@@ -1,10 +1,16 @@
 package com.czertainly.core.service;
 
+import com.czertainly.api.exception.NotFoundException;
+import com.czertainly.api.model.core.auth.Resource;
+import com.czertainly.api.model.core.other.ResourceEvent;
 import com.czertainly.api.model.core.settings.*;
+import com.czertainly.api.model.core.settings.authentication.AuthenticationSettingsUpdateDto;
 import com.czertainly.api.model.core.settings.authentication.OAuth2ProviderSettingsResponseDto;
 import com.czertainly.api.model.core.settings.authentication.OAuth2ProviderSettingsUpdateDto;
 import com.czertainly.core.dao.entity.Setting;
+import com.czertainly.core.dao.entity.workflows.Trigger;
 import com.czertainly.core.dao.repository.SettingRepository;
+import com.czertainly.core.dao.repository.workflows.TriggerRepository;
 import com.czertainly.core.service.impl.SettingServiceImpl;
 import com.czertainly.core.util.BaseSpringBootTest;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -15,19 +21,36 @@ import com.nimbusds.jose.jwk.gen.ECKeyGenerator;
 import com.nimbusds.jose.jwk.gen.OctetSequenceKeyGenerator;
 import com.nimbusds.jose.jwk.gen.RSAKeyGenerator;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.text.ParseException;
-import java.util.Base64;
-import java.util.List;
+import java.util.*;
 
 class SettingServiceTest extends BaseSpringBootTest {
 
+    private static final String TEST_TRIGGER_NAME = "testTriggerName";
+    private static final String TEST_TRIGGER_UUID = "3a1db3f5-f9eb-4fbf-92c9-c4c1499bfca7";
+
     @Autowired
     private SettingService settingService;
+
     @Autowired
     private SettingRepository settingRepository;
+
+    @Autowired
+    private TriggerRepository triggerRepository;
+
+    @BeforeEach
+    void setUp() {
+        Trigger trigger = new Trigger();
+        trigger.setUuid(UUID.fromString(TEST_TRIGGER_UUID));
+        trigger.setName(TEST_TRIGGER_NAME);
+        trigger.setResource(Resource.CERTIFICATE);
+
+        triggerRepository.save(trigger);
+    }
 
     @Test
     void updatePlatformSettings() {
@@ -99,6 +122,45 @@ class SettingServiceTest extends BaseSpringBootTest {
         Assertions.assertEquals(Base64.getEncoder().encodeToString(ecJwk.toPublicKey().getEncoded()), oAuth2ProvidersSettingsUpdateDto.getJwkSetKeys().stream().filter(jwk -> jwk.getKeyType().equals("EC")).findFirst().get().getPublicKey());
         Assertions.assertEquals(Base64.getEncoder().encodeToString(aesJwk.toByteArray()), oAuth2ProvidersSettingsUpdateDto.getJwkSetKeys().stream().filter(jwk -> jwk.getKeyType().equals("oct")).findFirst().get().getPublicKey());
         Assertions.assertEquals(Base64.getEncoder().encodeToString(octetKeyPairJwk.getDecodedX()), oAuth2ProvidersSettingsUpdateDto.getJwkSetKeys().stream().filter(jwk -> jwk.getKeyType().equals("OKP")).findFirst().get().getPublicKey());
+    }
+
+    @Test
+    void testUpdateAuthenticationSettings() {
+        AuthenticationSettingsUpdateDto authenticationSettingsUpdateDto = new AuthenticationSettingsUpdateDto();
+        authenticationSettingsUpdateDto.setDisableLocalhostUser(true);
+        authenticationSettingsUpdateDto.setOAuth2Providers(List.of());
+
+        Assertions.assertDoesNotThrow(() -> settingService.updateAuthenticationSettings(authenticationSettingsUpdateDto), "Updating authentication settings should not throw any exception");
+    }
+
+    @Test
+    void testUpdateEventSettings() {
+        EventsSettingsDto eventSettings = settingService.getEventsSettings();
+        Assertions.assertNotNull(eventSettings);
+
+        Assertions.assertDoesNotThrow(() -> settingService.updateEventsSettings(eventSettings), "Updating event settings should not throw any exception");
+
+        EventSettingsDto eventSettingsDto = new EventSettingsDto();
+        eventSettingsDto.setEvent(ResourceEvent.CERTIFICATE_DISCOVERED);
+        eventSettingsDto.setTriggerUuids(List.of(UUID.fromString(TEST_TRIGGER_UUID)));
+
+        Assertions.assertDoesNotThrow(() -> settingService.updateEventSettings(eventSettingsDto), "Updating event settings should not throw any exception");
+    }
+
+    @Test
+    void testUpdateNonExistingTriggerEventSettings() {
+        EventsSettingsDto eventsSettings = new EventsSettingsDto();
+        Map<ResourceEvent, List<UUID>> eventsMapping = new HashMap<>();
+        eventsMapping.put(ResourceEvent.CERTIFICATE_DISCOVERED, List.of(UUID.fromString("3a1db3f5-f9eb-4fbf-92c9-c4c1499bfca8")));
+        eventsSettings.setEventsMapping(eventsMapping);
+
+        Assertions.assertThrows(NotFoundException.class, () -> settingService.updateEventsSettings(eventsSettings));
+
+        EventSettingsDto eventSettingsDto = new EventSettingsDto();
+        eventSettingsDto.setEvent(ResourceEvent.CERTIFICATE_DISCOVERED);
+        eventSettingsDto.setTriggerUuids(List.of(UUID.fromString("3a1db3f5-f9eb-4fbf-92c9-c4c1499bfca8")));
+
+        Assertions.assertThrows(NotFoundException.class, () -> settingService.updateEventSettings(eventSettingsDto), "Updating non-existing trigger event settings should throw NotFoundException");
     }
 
 }
