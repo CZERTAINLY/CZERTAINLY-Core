dbe2108d833da2ba6cfbdb01aa350bbd5b96b1f0
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2025-05-22 09:31:45 +0200

    Change types of certificate filter fields Expires At and Valid From to Datetime (#1038)

--- src/main/java/com/czertainly/core/util/DatabaseMigration.java
+++ src/main/java/com/czertainly/core/util/DatabaseMigration.java
@@ -63,7 +63,8 @@ public class DatabaseMigration {
         V202408231055__DuplicateCertificates(-1714171989),
         V202411141900__CreateLocalhostUserAndPermissions(1923994081),
         V202501281511__LinkKeysToCertificates(-1169157739),
-        V202501281641__DuplicateCertificateContents(492594371)
+        V202501281641__DuplicateCertificateContents(492594371),
+        V202505211440__ChangeConditionValueForNotAfterAndNotBefore(-529789813)
         ;
 
         private final int checksum;
