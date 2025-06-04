88e865149ec6b6f86217c7d6820a4023b86448a3
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2025-06-04 15:20:01 +0200

    Update CertificateUtil.java

--- src/main/java/com/czertainly/core/util/CertificateUtil.java
+++ src/main/java/com/czertainly/core/util/CertificateUtil.java
@@ -534,7 +534,7 @@ public class CertificateUtil {
         return keyAlgorithm.name();
     }
 
-    @Deprecated()
+    @Deprecated(forRemoval = true)
     public static String getAlgorithmFromProviderName(String providerName) {
         // Used only in a past migration
         return null;
