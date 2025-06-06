4994cc6e425b822587b27fe9ee723e3b66dd2251
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2025-06-06 16:18:02 +0200

    Do not send internal notifications for expiring event

--- src/main/java/com/czertainly/core/events/handlers/CertificateExpiringEventHandler.java
+++ src/main/java/com/czertainly/core/events/handlers/CertificateExpiringEventHandler.java
@@ -3,17 +3,12 @@ package com.czertainly.core.events.handlers;
 import com.czertainly.api.model.core.auth.Resource;
 import com.czertainly.api.model.core.other.ResourceEvent;
 import com.czertainly.core.dao.entity.Certificate;
-import com.czertainly.core.dao.entity.UniquelyIdentifiedAndAudited;
 import com.czertainly.core.dao.repository.CertificateRepository;
 import com.czertainly.core.evaluator.CertificateTriggerEvaluator;
-import com.czertainly.core.events.EventContext;
 import com.czertainly.core.events.data.EventDataBuilder;
 import com.czertainly.core.messaging.model.EventMessage;
-import com.czertainly.core.messaging.model.NotificationMessage;
-import com.czertainly.core.messaging.model.NotificationRecipient;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.UUID;
 
 @SuppressWarnings("java:S6830")
@@ -27,16 +22,6 @@ public class CertificateExpiringEventHandler extends CertificateEventsHandler {
     @Override
     protected Object getEventData(Certificate object, Object eventMessageData) {
         return EventDataBuilder.getCertificateExpiringEventData(object);
-    }
-
-    @Override
-    protected void sendFollowUpEventsNotifications(EventContext<Certificate> eventContext) {
-        Certificate certificate = eventContext.getResourceObjects().getFirst();
-        Object eventData = eventContext.getResourceObjectsEventData().getFirst();
-
-        List<NotificationRecipient> recipients = NotificationRecipient.buildUsersAndGroupsNotificationRecipients(certificate.getOwner() == null ? null : List.of(certificate.getOwner().getUuid()), certificate.getGroups() == null ? null : certificate.getGroups().stream().map(UniquelyIdentifiedAndAudited::getUuid).toList());
-        NotificationMessage notificationMessage = new NotificationMessage(eventContext.getEvent(), Resource.CERTIFICATE, certificate.getUuid(), null, recipients, eventData);
-        notificationProducer.produceMessage(notificationMessage);
     }
 
     public static EventMessage constructEventMessages(UUID expiringCertificateUuid) {
