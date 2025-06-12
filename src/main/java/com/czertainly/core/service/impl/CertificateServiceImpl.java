42b5b25bbf397657471eae5901aa5c9d176dab5f
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2025-06-12 10:21:59 +0200

    Update CertificateServiceImpl.java

--- src/main/java/com/czertainly/core/service/impl/CertificateServiceImpl.java
+++ src/main/java/com/czertainly/core/service/impl/CertificateServiceImpl.java
@@ -64,6 +64,7 @@ import org.bouncycastle.cert.jcajce.JcaCertStore;
 import org.bouncycastle.cms.CMSProcessableByteArray;
 import org.bouncycastle.cms.CMSSignedDataGenerator;
 import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
+import org.bouncycastle.operator.DefaultAlgorithmNameFinder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MarkerFactory;
@@ -1417,8 +1418,7 @@ public class CertificateServiceImpl implements CertificateService {
             certificateRequestEntity = certificate.prepareCertificateRequest(certificateRequestFormat);
             certificateRequestEntity.setFingerprint(certificateRequestFingerprint);
             certificateRequestEntity.setContent(certificateRequest);
-            certificateRequestEntity.setSignatureAlgorithm(request.getSignatureAlgorithm().getAlgorithm().toString());
-            certificateRequestEntity = certificateRequestRepository.save(certificateRequestEntity);
+            setCertificateRequestEntitySignatureAlgorithms(request, certificateRequestEntity);
 
             requestAttributes = attributeEngine.updateObjectDataAttributesContent(
                     null, null, Resource.CERTIFICATE_REQUEST, certificateRequestEntity.getUuid(), csrAttributes
@@ -1441,7 +1441,6 @@ public class CertificateServiceImpl implements CertificateService {
             certificateRequestEntity.setAltKeyUuid(altKeyUuid);
         else if (request.getAltPublicKey() != null) {
             setCertificateRequestAltKey(certificateRequestEntity, request.getAltPublicKey());
-            certificateRequestEntity.setAltSignatureAlgorithm(request.getAltSignatureAlgorithm().getAlgorithm().toString());
         }
 
         certificate.setCertificateRequest(certificateRequestEntity);
@@ -1479,6 +1478,14 @@ public class CertificateServiceImpl implements CertificateService {
         logger.info("Certificate request submitted and certificate created {}", certificate);
 
         return dto;
+    }
+
+    private static void setCertificateRequestEntitySignatureAlgorithms(CertificateRequest request, CertificateRequestEntity certificateRequestEntity) {
+        DefaultAlgorithmNameFinder algFinder = new DefaultAlgorithmNameFinder();
+        if (request.getSignatureAlgorithm() != null)
+            certificateRequestEntity.setSignatureAlgorithm(algFinder.getAlgorithmName(request.getSignatureAlgorithm()).replace("WITH", "with"));
+        if (request.getAltSignatureAlgorithm() != null)
+            certificateRequestEntity.setAltSignatureAlgorithm(algFinder.getAlgorithmName(request.getAltSignatureAlgorithm()).replace("WITH", "with"));
     }
 
     private UUID getCertificateRequestKey(CertificateRequestEntity certificateRequest, PublicKey csrPublicKey) throws NoSuchAlgorithmException {
