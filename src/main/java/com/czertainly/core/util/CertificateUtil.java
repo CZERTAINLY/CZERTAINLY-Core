535f09423675bae0f44241e9b98e4b32b19834df
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2025-08-26 10:35:31 +0200

    Use int bitmask for Key Usage

--- src/main/java/com/czertainly/core/util/CertificateUtil.java
+++ src/main/java/com/czertainly/core/util/CertificateUtil.java
@@ -122,18 +122,14 @@ public class CertificateUtil {
         return Base64.getEncoder().encodeToString(certificate.getEncoded());
     }
 
-    public static List<String> keyUsageExtractor(boolean[] keyUsage) {
-        List<String> keyUsageNames = new ArrayList<>();
-        if (keyUsage == null) {
-            return keyUsageNames;
-        }
-
+    public static int keyUsageExtractor(boolean[] keyUsage) {
+        int result = 0;
         for (int i = 0; i < keyUsage.length; i++) {
-            if (Boolean.TRUE.equals(keyUsage[i])) {
-                keyUsageNames.add(CertificateKeyUsage.fromIndex(i).getCode());
+            if (keyUsage[i]) {
+                result |= (1 << i);  // set bit i
             }
         }
-        return keyUsageNames;
+        return result;
     }
 
     public static boolean isKeyUsagePresent(boolean[] keyUsage, CertificateKeyUsage keyUsageName) {
@@ -424,7 +420,7 @@ public class CertificateUtil {
         }
         if (extendedKeyUsage != null) modal.setExtendedKeyUsage(MetaDefinitions.serializeArrayString(extendedKeyUsage));
         modal.setKeyUsage(
-               CertificateUtil.keyUsageExtractor(certificate.getKeyUsage()).stream().map(CertificateKeyUsage::fromCode).toList());
+               CertificateUtil.keyUsageExtractor(certificate.getKeyUsage()));
         modal.setSubjectType(subjectType);
         // Set trusted certificate mark either for CA or for self-signed certificate
         if (subjectType != CertificateSubjectType.END_ENTITY)
