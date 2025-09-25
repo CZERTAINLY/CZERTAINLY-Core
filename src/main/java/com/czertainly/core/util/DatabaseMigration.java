64bcfcd32ce0a90f07981aca848ecc9119b2913f
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2025-09-25 11:56:58 +0200

    Update DatabaseMigration.java

--- src/main/java/com/czertainly/core/util/DatabaseMigration.java
+++ src/main/java/com/czertainly/core/util/DatabaseMigration.java
@@ -71,7 +71,7 @@ public class DatabaseMigration {
         V202508261555__EnumCollectionsColumnsBitmask(-1617046736),
         V202508281320__UniqueCryptographicKeyItemFingerprint(2020026258),
         V202509041555__CertificateRequestEntityBitmask(1876867908),
-        V202509191412__LogRecordsRefactor(674763547);
+        V202509191412__LogRecordsRefactor(79840308);
 
         private final int checksum;
 
