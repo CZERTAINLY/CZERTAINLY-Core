c96e0e7f84e372ef288b99d52bbc5e0a0f1cccc1
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2025-09-30 14:38:10 +0200

    Update AttributeEngine.java

--- src/main/java/com/czertainly/core/attribute/engine/AttributeEngine.java
+++ src/main/java/com/czertainly/core/attribute/engine/AttributeEngine.java
@@ -1062,12 +1062,13 @@ public class AttributeEngine {
                     } catch (IllegalArgumentException e) {
                         throw new AttributeException("Malformed attribute content data: " + e.getMessage(), attributeDefinition.getUuid().toString(), attributeDefinition.getName(), attributeDefinition.getType(), connectorUuidStr);
                     }
-                    List<ValidationError> constraintsValidationErrors = AttributeDefinitionUtils.validateConstraints(attributeDefinition.getDefinition(), attributeContent);
-                    if (!constraintsValidationErrors.isEmpty()) {
-                        throw new AttributeException(constraintsValidationErrors.stream()
-                                .map(ValidationError::getErrorDescription)
-                                .collect(Collectors.joining(" \n")), attributeDefinition.getUuid().toString(), attributeDefinition.getName(), attributeDefinition.getType(), connectorUuidStr);
-                    }
+                }
+
+                List<ValidationError> constraintsValidationErrors = AttributeDefinitionUtils.validateConstraints(attributeDefinition.getDefinition(), attributeContent);
+                if (!constraintsValidationErrors.isEmpty()) {
+                    throw new AttributeException(constraintsValidationErrors.stream()
+                            .map(ValidationError::getErrorDescription)
+                            .collect(Collectors.joining(" \n")), attributeDefinition.getUuid().toString(), attributeDefinition.getName(), attributeDefinition.getType(), connectorUuidStr);
                 }
 
                 // convert content items to its respective content classes
