665c66533fcba1c3d69893e6d4cb2faef2c75f72
lubomirw <76479559+lubomirw@users.noreply.github.com>
2025-11-06 16:22:18 +0100

    Remove unused RabbitMQ imports

--- src/main/java/com/czertainly/core/messaging/configuration/RabbitMQConfiguration.java
+++ src/main/java/com/czertainly/core/messaging/configuration/RabbitMQConfiguration.java
@@ -4,8 +4,6 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
