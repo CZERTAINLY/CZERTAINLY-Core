13a68fc1137cec1e94fcb511ff820338a591a7eb
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2025-12-04 08:44:13 +0100

    Add migration

--- src/main/resources/db/migration/V202511241530__attributes_v3.sql
+++ src/main/resources/db/migration/V202511241530__attributes_v3.sql
@@ -14,22 +14,88 @@ SET definition =
         ),
         false
     )
-WHERE ad.type = 'CUSTOM';
+WHERE ad.type = 'CUSTOM' AND ad.definition ? 'content' AND jsonb_array_length(ad.definition->'content') > 0;
 
-UPDATE attribute_content_item aci JOIN attribute_definition ad ON aci.attribute_definition_uuid = ad.uuid
-SET json =
-    jsonb_set(
-        '{content}',
-        (
-            SELECT jsonb_agg(
-                elem || jsonb_build_object('contentType', LOWER(ad.content_type))
-            )
-            FROM jsonb_array_elements(aci.json->'content') AS elem
-        ),
-        false
+UPDATE attribute_content_item aci
+SET json = jsonb_set(
+        aci.json,
+        '{contentType}',
+        to_jsonb(LOWER(ad.content_type)),
+        true
     )
-WHERE ad.type = 'CUSTOM';
+FROM attribute_definition ad
+WHERE aci.attribute_definition_uuid = ad.uuid
+  AND ad.type = 'CUSTOM';
+
 
 -- migrate custom attributes where they are only as a json column
 
+UPDATE execution_item ei
+SET data = (
+    SELECT jsonb_agg(
+               elem || jsonb_build_object(
+                   'contentType', LOWER(SPLIT_PART(ei.field_identifier, '|', 2))
+               )
+           )
+    FROM jsonb_array_elements(ei.data) AS elem
+)
+WHERE field_source = 'CUSTOM';
+
+UPDATE protocol_certificate_associations
+SET custom_attributes = (
+  SELECT jsonb_agg(
+           outer_elem
+           || jsonb_build_object(
+                'version', '3',
+                'content',
+                CASE
+                  WHEN jsonb_typeof(outer_elem->'content') = 'array'
+                  THEN COALESCE(
+                         (
+                           SELECT jsonb_agg(
+                                    inner_elem || jsonb_build_object(
+                                      'contentType', outer_elem->>'contentType'
+                                    )
+                                  )
+                           FROM jsonb_array_elements(outer_elem->'content') AS inner_elem
+                           WHERE inner_elem IS NOT NULL
+                         ),
+                         '[]'::jsonb
+                       )
+                  ELSE outer_elem->'content'
+                END
+              )
+         )
+  FROM jsonb_array_elements(custom_attributes) AS outer_elem
+)
+WHERE custom_attributes != '[]';
+
+UPDATE compliance_profile_rule
+SET attributes = (
+  SELECT jsonb_agg(
+           outer_elem
+           || jsonb_build_object(
+                'version', '3',
+                'content',
+                CASE
+                  WHEN jsonb_typeof(outer_elem->'content') = 'array'
+                  THEN COALESCE(
+                         (
+                           SELECT jsonb_agg(
+                                    inner_elem || jsonb_build_object(
+                                      'contentType', outer_elem->>'contentType'
+                                    )
+                                  )
+                           FROM jsonb_array_elements(outer_elem->'content') AS inner_elem
+                           WHERE inner_elem IS NOT NULL
+                         ),
+                         '[]'::jsonb
+                       )
+                  ELSE outer_elem->'content'
+                END
+              )
+         )
+  FROM jsonb_array_elements(attributes) AS outer_elem
+)
+WHERE attributes != '[]';
 
