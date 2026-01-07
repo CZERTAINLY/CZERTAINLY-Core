4a101fd35df7391d02b457647ef6fac5ee561087
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2026-01-07 15:19:38 +0100

    Update SearchFieldObject.java

--- src/main/java/com/czertainly/core/model/SearchFieldObject.java
+++ src/main/java/com/czertainly/core/model/SearchFieldObject.java
@@ -1,9 +1,16 @@
 package com.czertainly.core.model;
 
-import com.czertainly.api.model.common.attribute.common.AttributeType;
+import com.czertainly.api.model.common.attribute.common.*;
+import com.czertainly.api.model.common.attribute.v2.DataAttributeV2;
 import com.czertainly.api.model.common.attribute.common.content.AttributeContentType;
+import com.czertainly.api.model.common.attribute.v3.CustomAttributeV3;
+import com.czertainly.api.model.common.attribute.v3.DataAttributeV3;
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Data
@@ -28,4 +35,76 @@ public class SearchFieldObject {
         this.attributeContentType = attributeContentType;
     }
 
+    public SearchFieldObject(String attributeName, AttributeContentType attributeContentType, AttributeType attributeType) {
+        this.attributeName = attributeName;
+        this.attributeContentType = attributeContentType;
+        this.attributeType = attributeType;
+    }
+
+    public SearchFieldObject(String attributeName, AttributeContentType attributeContentType, AttributeType attributeType, String label, BaseAttribute attributeDefinition) {
+        this.attributeName = attributeName;
+        this.attributeContentType = attributeContentType;
+        this.attributeType = attributeType;
+        this.label = label;
+
+        if (attributeType == AttributeType.CUSTOM || attributeType == AttributeType.DATA) {
+            if (attributeDefinition instanceof CustomAttribute customAttribute) {
+                list = customAttribute.getProperties().isList();
+                multiSelect = customAttribute.getProperties().isMultiSelect();
+                if (list && customAttribute.getContent() != null) {
+                    contentItems = ((List<? extends AttributeContent>) customAttribute.getContent()).stream().map(item -> item.getData().toString()).toList();
+                }
+            } else {
+                DataAttribute dataAttribute = (DataAttribute) attributeDefinition;
+                // data attributes that are list can have content provided later by callback so do not mark it as list if content is empty
+                List<? extends AttributeContent> content = dataAttribute.getContent();
+                list = dataAttribute.getProperties().isList() && content != null && !content.isEmpty();
+                multiSelect = dataAttribute.getProperties().isMultiSelect();
+                if (list) {
+                    contentItems = content.stream().map(item -> item.getData().toString()).toList();
+                }
+            }
+        }
+    }
+
+    public boolean isDateTimeFormat() {
+        return this.attributeContentType.equals(AttributeContentType.DATE)
+                || this.attributeContentType.equals(AttributeContentType.TIME)
+                || this.attributeContentType.equals(AttributeContentType.DATETIME);
+    }
+
+    public boolean isBooleanFormat() {
+        return this.attributeContentType.equals(AttributeContentType.BOOLEAN);
+    }
+
+    public Class getDateTimeFormatClass() {
+        Class clazz = null;
+        switch (this.attributeContentType) {
+            case DATE -> clazz = LocalDate.class;
+            case TIME -> clazz = LocalTime.class;
+            case DATETIME -> clazz = LocalDateTime.class;
+        }
+        return clazz;
+    }
+
+    public LocalDateTime getLocalDateTimeFormat(final String dateTimeValue) {
+        if (!this.attributeContentType.equals(AttributeContentType.DATETIME)) {
+            return null;
+        }
+        return LocalDateTime.parse(dateTimeValue, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX"));
+    }
+
+    public LocalDate getLocalDateFormat(final String dateTimeValue) {
+        if (!this.attributeContentType.equals(AttributeContentType.DATE)) {
+            return null;
+        }
+        return LocalDate.parse(dateTimeValue);
+    }
+
+    public LocalTime getLocalTimeFormat(final String dateTimeValue) {
+        if (!this.attributeContentType.equals(AttributeContentType.TIME)) {
+            return null;
+        }
+        return LocalTime.parse(dateTimeValue);
+    }
 }
