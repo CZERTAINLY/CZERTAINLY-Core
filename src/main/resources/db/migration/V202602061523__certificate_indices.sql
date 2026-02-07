018ff003d9e0c741dbf0617c6b87e0d6650b8797
Ivo Raisr <ivosh@ivosh.net>
2026-02-08 00:33:30 +0100

    Do not explicitly specify database schema as it is configurable (#1260)

--- src/main/resources/db/migration/V202602061523__certificate_indices.sql
+++ src/main/resources/db/migration/V202602061523__certificate_indices.sql
@@ -1,17 +1,17 @@
 create index certificate_signature_algorithm
-    on core.certificate (signature_algorithm);
+    on certificate (signature_algorithm);
 
 create index certificate_alt_signature_algorithm
-    on core.certificate (alt_signature_algorithm);
+    on certificate (alt_signature_algorithm);
 
 create index certificate_public_key_algorithm
-    on core.certificate (public_key_algorithm);
+    on certificate (public_key_algorithm);
 
 create index certificate_alt_public_key_algorithm
-    on core.certificate (alt_public_key_algorithm);
+    on certificate (alt_public_key_algorithm);
 
 create index certificate_key_size
-    on core.certificate (key_size);
+    on certificate (key_size);
 
 create index certificate_alt_key_size
-    on core.certificate (alt_key_size);
+    on certificate (alt_key_size);
