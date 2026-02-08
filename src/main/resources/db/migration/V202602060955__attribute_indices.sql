797ed996b293c620c79ec900f7789abfac3a7da0
Ivo Raisr <ivosh@ivosh.net>
2026-02-08 16:17:27 +0100

    Optimize performance of Certificate Search Filter in attribute_content_2_object table (#1263)

---
+++ src/main/resources/db/migration/V202602060955__attribute_indices.sql
@@ -0,0 +1,8 @@
+create index attribute_content_2_object_object_type
+    on attribute_content_2_object (object_type);
+
+create index attribute_definition_type
+    on attribute_definition (type);
+
+create index attribute_relation_resource
+    on attribute_relation (resource);
