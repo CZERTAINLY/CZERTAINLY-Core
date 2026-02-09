131007a1b3c82703c0031e1e8ca159e079c29e6e
Michal Vyskocil <michal.vyskocil@3key.company>
2026-02-09 12:24:26 +0100

    Use cbom repo exception from interfaces

--- src/main/java/com/czertainly/core/api/web/CbomControllerImpl.java
+++ src/main/java/com/czertainly/core/api/web/CbomControllerImpl.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.czertainly.api.exception.AlreadyExistException;
 import com.czertainly.api.exception.AttributeException;
 import com.czertainly.api.exception.NotFoundException;
+import com.czertainly.api.exception.ValidationException;
 import com.czertainly.api.interfaces.core.web.CbomController;
 import com.czertainly.api.model.client.certificate.SearchRequestDto;
 import com.czertainly.api.model.core.auth.Resource;
@@ -15,10 +17,11 @@ import com.czertainly.api.model.core.cbom.CbomDto;
 import com.czertainly.api.model.core.cbom.CbomListResponseDto;
 import com.czertainly.api.model.core.cbom.CbomUploadRequestDto;
 import com.czertainly.api.model.core.logging.enums.Operation;
+import com.czertainly.api.model.core.search.SearchFieldDataByGroupDto;
 import com.czertainly.api.model.core.logging.enums.Module;
 import com.czertainly.core.aop.AuditLogged;
 import com.czertainly.core.auth.AuthEndpoint;
-import com.czertainly.core.cbom.client.CbomRepositoryException;
+import com.czertainly.api.exception.CbomRepositoryException;
 import com.czertainly.core.security.authz.SecuredUUID;
 import com.czertainly.core.service.CbomService;
 
@@ -43,17 +46,15 @@ public class CbomControllerImpl implements CbomController {
     }
 
     @Override
-    @AuthEndpoint(resourceName = Resource.CBOM)
     @AuditLogged(
         module = Module.CORE,
         resource = Resource.CBOM,
         operation = Operation.DETAIL)
-    public CbomDetailDto getCbomDetail(String uuid, String version)	throws NotFoundException, CbomRepositoryException {
+    public CbomDetailDto getCbomDetail(String uuid, String version) throws NotFoundException, CbomRepositoryException {
         return cbomService.getCbomDetail(SecuredUUID.fromString(uuid));
     }
 
     @Override
-    @AuthEndpoint(resourceName = Resource.CBOM)
     @AuditLogged(
         module = Module.CORE,
         resource = Resource.CBOM,
@@ -63,13 +64,23 @@ public class CbomControllerImpl implements CbomController {
     }
 
     @Override
-    @AuthEndpoint(resourceName = Resource.CBOM)
     @AuditLogged(
         module = Module.CORE,
         resource = Resource.CBOM,
         operation = Operation.CREATE)
-	public CbomDto uploadCbom(CbomUploadRequestDto request) throws AttributeException {
+	public CbomDto uploadCbom(CbomUploadRequestDto request) throws ValidationException, AlreadyExistException, CbomRepositoryException {
         CbomDto cbomDto = cbomService.createCbom(request);
         return cbomDto;
+    }
+
+    @Override
+    @AuditLogged(
+        module = Module.CORE,
+        resource = Resource.SEARCH_FILTER,
+        affiliatedResource = Resource.CBOM,
+        operation = Operation.LIST
+    )
+    public List<SearchFieldDataByGroupDto> getSearchableFieldInformation(){
+        return cbomService.getSearchableFieldInformationByGroup();
     }
 }
