5718f0641233f2bb9f26128f5ece44f97980bb8a
lubomirw <76479559+lubomirw@users.noreply.github.com>
2026-02-10 22:24:04 +0100

    Add Build info actuator contributor from Git properties of maven plugin
    - include Git metadata during build and expose it via Actuator info in SNAPSHOT version
    - add .git into the prebuild Docker stage
    - add a Windows build helper (build_image_locally_win.bat) to run the prebuild container and assemble the final image

---
+++ build_image_locally_win.bat
@@ -0,0 +1,66 @@
+@echo off
+
+REM Name of the image to build (including tag)
+set IMAGE_NAME=3keycompany/czertainly-core:develop-latest
+REM Dockerfile path for the main image
+set DOCKERFILE_PATH=Dockerfile
+
+REM Maven directory that will be mounted to the container to cache dependencies
+REM If not set, the container will download dependencies on every build
+REM e.g. C:\Users\username\.m2, make sure to use the correct path for your system
+set MAVEN_DIR=C:\Users\%USERNAME%\.m2
+REM true to skip tests, default is false
+set SKIP_TESTS=false
+
+echo Build of CZERTAINLY CORE
+
+echo Cleaning containers
+docker rm czertainlycont
+docker rmi prebuild
+
+echo Refreshing directory for data from container
+if exist data rmdir /s /q data
+mkdir data
+
+echo PreBuild build
+docker build --build-arg SERVER_USERNAME=%SERVER_USERNAME% --build-arg SERVER_PASSWORD=%SERVER_PASSWORD% -f %DOCKERFILE_PATH%-pre -t prebuild .
+
+echo MVN Build
+
+REM Construct optional arguments
+set MAVEN_VOLUME_ARG=
+set SKIP_TESTS_ARG=
+
+if defined MAVEN_DIR (
+  set MAVEN_VOLUME_ARG=-v %MAVEN_DIR%:/root/.m2
+  echo Maven directory detected: %MAVEN_DIR%
+)
+
+if "%SKIP_TESTS%"=="true" (
+  set SKIP_TESTS_ARG=-DskipTests
+  echo Skipping tests as SKIP_TESTS is true
+) else (
+  echo Running tests as SKIP_TESTS is not true or unset (default: false)
+)
+
+REM Windows uses named pipe for Docker socket
+docker run -v //var/run/docker.sock:/var/run/docker.sock ^
+  %MAVEN_VOLUME_ARG% ^
+  --name czertainlycont -i prebuild ^
+  mvn -f /home/app/pom.xml clean package %SKIP_TESTS_ARG%
+
+echo Starting czertainlycont
+docker start czertainlycont
+
+echo Copy jar back to Tmp
+docker cp czertainlycont:/home/app/target data\target
+docker cp czertainlycont:/home/app/docker data\docker
+
+echo Removing previous image
+docker rmi %IMAGE_NAME%
+
+echo Build main image
+docker build --platform linux/amd64 -f %DOCKERFILE_PATH% -t %IMAGE_NAME% .
+
+echo Cleaning workspace
+rmdir /s /q data
