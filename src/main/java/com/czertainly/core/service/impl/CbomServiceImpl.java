c570b041df64a50d4a4bd8eae484322309e80a1e
Michal Vyskocil <michal.vyskocil@3key.company>
2026-02-12 21:15:32 +0100

    add Lazy as suggested by AI

--- src/main/java/com/czertainly/core/service/impl/CbomServiceImpl.java
+++ src/main/java/com/czertainly/core/service/impl/CbomServiceImpl.java
@@ -47,6 +47,7 @@ import org.apache.commons.lang3.function.TriFunction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -67,6 +68,7 @@ public class CbomServiceImpl implements CbomService {
 
     private CbomRepositoryClient cbomRepositoryClient;
 
+    @Lazy
     private AttributeEngine attributeEngine;
 
     @Autowired
