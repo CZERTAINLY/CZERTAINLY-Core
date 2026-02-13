65222a07592c3f6854d00efa77a11cdd726d9fff
Michal Vyskocil <michal.vyskocil@3key.company>
2026-02-13 15:21:41 +0100

    remove the hand written sql - java is supposed to generate it

--- src/main/java/com/czertainly/core/dao/repository/CbomRepository.java
+++ src/main/java/com/czertainly/core/dao/repository/CbomRepository.java
@@ -3,7 +3,6 @@ package com.czertainly.core.dao.repository;
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -12,20 +11,5 @@ import com.czertainly.core.dao.entity.Cbom;
 @Repository
 public interface CbomRepository extends SecurityFilterRepository<Cbom, UUID> {
 
-    @Query(value = """
-        SELECT
-            uuid,
-            serial_number,
-            version,
-            created_at,
-            source,
-            algorithms_count,
-            certificates_count,
-            protocols_count,
-            crypto_material_count,
-            total_assets_count
-        FROM cbom c
-        WHERE c.serial_number = :serial_number
-    """, nativeQuery = true)
     List<Cbom> findBySerialNumber(@Param("serial_number") String serialNumber);
 }
