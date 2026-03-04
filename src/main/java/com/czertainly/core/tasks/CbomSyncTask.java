c520fee4deea2b292d1c3fb18d109a098a336301
Michal Vyskocil <michal.vyskocil@3key.company>
2026-03-04 14:28:50 +0100

    removed unused imports

--- src/main/java/com/czertainly/core/tasks/CbomSyncTask.java
+++ src/main/java/com/czertainly/core/tasks/CbomSyncTask.java
@@ -1,22 +1,15 @@
 package com.czertainly.core.tasks;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import com.czertainly.api.model.core.auth.Resource;
 import com.czertainly.api.model.scheduler.SchedulerJobExecutionStatus;
 import com.czertainly.core.model.ScheduledTaskResult;
 import com.czertainly.core.service.CbomService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.NoArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 @Component
 @NoArgsConstructor
