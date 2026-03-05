51b304c3ef23ac84e8c0bb5c595fb36b56baf0ff
Michal Vyskocil <michal.vyskocil@3key.company>
2026-03-05 15:28:25 +0100

    drop unused import

--- src/test/java/com/czertainly/core/tasks/CbomSyncTaskTest.java
+++ src/test/java/com/czertainly/core/tasks/CbomSyncTaskTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
