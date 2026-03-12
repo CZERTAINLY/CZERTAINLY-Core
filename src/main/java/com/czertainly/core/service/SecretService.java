df519194878318582938f7ea46e1002a9b221c45
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2026-03-12 13:25:09 +0100

    Add missing calls to prepare request attributes for connector in Secret Service (#1333)

--- src/main/java/com/czertainly/core/service/SecretService.java
+++ src/main/java/com/czertainly/core/service/SecretService.java
@@ -14,7 +14,6 @@ import com.czertainly.core.security.authz.SecuredParentUUID;
 import com.czertainly.core.security.authz.SecuredUUID;
 import com.czertainly.core.security.authz.SecurityFilter;
 
-import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.UUID;
 
@@ -28,7 +27,7 @@ public interface SecretService extends ResourceExtensionService {
 
     SecretDetailDto updateSecret(UUID uuid, SecretUpdateRequestDto secretRequest) throws NotFoundException, AttributeException, ConnectorException;
 
-    void deleteSecret(UUID uuid) throws NotFoundException, ConnectorException;
+    void deleteSecret(UUID uuid) throws NotFoundException, ConnectorException, AttributeException;
 
     void enableSecret(UUID uuid) throws NotFoundException;
 
@@ -36,13 +35,13 @@ public interface SecretService extends ResourceExtensionService {
 
     void addVaultProfileToSecret(UUID uuid, UUID vaultProfileUuid, List<RequestAttribute> createSecretAttributes) throws NotFoundException, ConnectorException, AttributeException;
 
-    void removeVaultProfileFromSecret(UUID uuid, UUID vaultProfileUuid) throws NotFoundException, ConnectorException;
+    void removeVaultProfileFromSecret(UUID uuid, UUID vaultProfileUuid) throws NotFoundException, ConnectorException, AttributeException;
 
     SecretDetailDto getSecretDetails(UUID uuid) throws NotFoundException;
 
     List<SecretVersionDto> getSecretVersions(UUID uuid) throws NotFoundException;
 
-    SecretContent getSecretContent(UUID uuid) throws NotFoundException, ConnectorException, NoSuchAlgorithmException;
+    SecretContent getSecretContent(UUID uuid) throws NotFoundException, ConnectorException, AttributeException;
 
     void updateSecretObjects(UUID uuid, SecretUpdateObjectsDto request) throws NotFoundException, ConnectorException, AttributeException;
 }
