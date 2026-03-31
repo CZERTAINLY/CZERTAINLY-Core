5f3045d2d7fdf9fe8727e7b956c7f9b91955a022
Ivo Raisr <ivosh@ivosh.net>
2026-03-31 14:06:26 +0200

    Add enabled flag to ILM and TSP configuration detail DTO

--- src/main/java/com/czertainly/core/mapper/signing/TspConfigurationMapper.java
+++ src/main/java/com/czertainly/core/mapper/signing/TspConfigurationMapper.java
@@ -17,6 +17,7 @@ public class TspConfigurationMapper {
         dto.setUuid(configuration.getUuid().toString());
         dto.setName(configuration.getName());
         dto.setDescription(configuration.getDescription());
+        dto.setEnabled(configuration.getEnabled() != null ? configuration.getEnabled() : false);
         dto.setCustomAttributes(customAttributes);
         return dto;
     }
