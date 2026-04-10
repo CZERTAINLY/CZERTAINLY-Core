cd65a39ffd0f81d7e254227384b86710288b2474
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2026-04-10 10:15:09 +0200

    Update SpringBootTestContext.java

--- src/test/java/com/czertainly/core/util/SpringBootTestContext.java
+++ src/test/java/com/czertainly/core/util/SpringBootTestContext.java
@@ -3,12 +3,12 @@ package com.czertainly.core.util;
 import com.czertainly.core.messaging.producers.AuditLogsProducer;
 import com.czertainly.core.security.authz.opa.OpaClient;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @TestConfiguration
 class SpringBootTestContext {
-    
-    @MockitoBean
+    @MockBean
     OpaClient opaClient;
 
     @MockitoBean
