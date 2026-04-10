c46ab466286e75e4bb8bc9e69184e8d4a1d9860d
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2026-04-10 10:08:56 +0200

    Add bean to context

--- src/test/java/com/czertainly/core/util/SpringBootTestContext.java
+++ src/test/java/com/czertainly/core/util/SpringBootTestContext.java
@@ -1,11 +1,16 @@
 package com.czertainly.core.util;
 
+import com.czertainly.core.messaging.producers.AuditLogsProducer;
 import com.czertainly.core.security.authz.opa.OpaClient;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @TestConfiguration
 class SpringBootTestContext {
-    @MockBean
+    
+    @MockitoBean
     OpaClient opaClient;
+
+    @MockitoBean
+    AuditLogsProducer auditLogsProducer;
 }
