257f75fe7e0dc5cd2c4a126ba9564721509c1ff9
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2026-04-10 14:01:54 +0200

    Update BaseSpringBootTestNoAuth.java

--- src/test/java/com/czertainly/core/util/BaseSpringBootTestNoAuth.java
+++ src/test/java/com/czertainly/core/util/BaseSpringBootTestNoAuth.java
@@ -6,7 +6,6 @@ import com.czertainly.core.security.authz.opa.dto.OpaResourceAccessResult;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.util.List;
