6a7f5fbd2fe9d336f11244438815d6c064781f64
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2026-04-10 11:04:16 +0200

    Try using only deprecated mockbean

--- src/test/java/com/czertainly/core/aop/AuditLogAspectTest.java
+++ src/test/java/com/czertainly/core/aop/AuditLogAspectTest.java
@@ -28,6 +28,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.util.List;
@@ -48,7 +49,7 @@ class AuditLogAspectTest extends BaseSpringBootTest {
     @Autowired
     private CryptographicKeyController keyController;
 
-    @Autowired
+    @MockBean
     private AuditLogsProducer auditLogsProducer;
 
     @Autowired
--- src/test/java/com/czertainly/core/util/SpringBootTestContext.java
+++ src/test/java/com/czertainly/core/util/SpringBootTestContext.java
@@ -1,16 +1,11 @@
 package com.czertainly.core.util;
 
-import com.czertainly.core.messaging.producers.AuditLogsProducer;
 import com.czertainly.core.security.authz.opa.OpaClient;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @TestConfiguration
 class SpringBootTestContext {
     @MockBean
     OpaClient opaClient;
-
-    @MockBean
-    AuditLogsProducer auditLogsProducer;
 }
