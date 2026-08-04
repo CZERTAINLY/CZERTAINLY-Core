9380926a23e5df7873c3c8203b1c2b947bc15345
3keyroman <46850604+3keyroman@users.noreply.github.com>
2026-08-04 12:54:10 +0200

    Remove an unused Mockito import left by the static-import switch

--- src/test/java/com/otilm/core/service/impl/NotificationProfileServiceImplPersistEditTest.java
+++ src/test/java/com/otilm/core/service/impl/NotificationProfileServiceImplPersistEditTest.java
@@ -17,7 +17,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.dao.DataIntegrityViolationException;
 
