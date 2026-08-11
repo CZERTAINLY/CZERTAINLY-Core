6cef3925a0591c94fb9bfe445f97edfbe8e2d172
klaraf755 <80590912+klaraf755@users.noreply.github.com>
2026-08-11 12:45:57 +0200

    Add missing import

--- src/test/java/com/otilm/core/util/CertificateUtilTest.java
+++ src/test/java/com/otilm/core/util/CertificateUtilTest.java
@@ -44,6 +44,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
